6292a09cbe0634e9a37767f0fe6a70f284018a12
Jed Perkins <perkins.jed@gmail.com>
2018-06-12 21:29:46 -0700

    Add hdf5 conversion helper functions

---
+++ mesoscope_2p/conversion_utils.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+from .tiff import MesoscopeTiff
+
+
+def dump_dict_as_attrs(h5fp, container_name, data):
+    if container_name is None:
+        container = h5fp
+    elif container_name in h5fp:
+        raise AttributeError("{} already exists".format(container_name))
+    else:
+        container = h5fp.create_group(container_name)
+    for key, value in data.items():
+        if isinstance(value, dict):
+            dump_dict_as_attrs(container, key, value)
+        else:
+            np_value = np.array(value)
+            if np_value.dtype.type == np.unicode_:
+                np_value = np_value.astype("S")
+            container.attrs.create(key, data=np_value)
+
+
+def scanfield_to_h5(h5fp, dset_name, scanfield_data, scanfield_metadata,
+                    page_block_size=None, **h5_opts):
+    if page_block_size is None:
+        dset = h5fp.create_dataset(dset_name, data=scanfield_data[:],
+                                   **h5_opts)
+    else:
+        dset = h5fp.create_dataset(dset_name, scanfield_data.shape,
+                                   dtype=scanfield_data.dtype, **h5_opts)
+        i = 0
+        while i < scanfield_data.shape[0]:
+            dset[i:i+page_block_size,:,:] = scanfield_data[i:i+page_block_size]
+            i += page_block_size
+    dump_dict_as_attrs(dset, None, scanfield_metadata)

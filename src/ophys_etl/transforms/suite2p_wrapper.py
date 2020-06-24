b7ed2d90b3ebce99681fdbff6db36d70b0879e0d
Dan <danielk@alleninstitute.org>
2020-06-24 12:10:22 -0700

    more exposition in schema for parameter choice.

--- src/ophys_etl/transforms/suite2p_wrapper.py
+++ src/ophys_etl/transforms/suite2p_wrapper.py
@@ -14,29 +14,39 @@ class Suite2PWrapperException(Exception):
 
 
 class Suite2PWrapperSchema(argschema.ArgSchema):
-    # Options for running s2p. One list can be found at:
-    # https://github.com/MouseLand/suite2p/blob/master/suite2p/run_s2p.py
+    """
+    s2p parameter names are copied from:
+    https://github.com/MouseLand/suite2p/blob/master/suite2p/run_s2p.py
+    descriptions here should indicate why certain default setting
+    choices have been made for Allen production.
+    """
 
     # s2p IO settings (file paths)
     h5py = argschema.fields.InputFile(
             required=True,
-            description="Path to 2P motion corrected hdf5 data path.")
+            description=("Path to input video. In Allen production case, "
+                         "assumed to be motion-corrected."))
     h5py_key = argschema.fields.Str(
             required=False,
             missing="data",
             default="data",
-            description="hdf5 file key for motion corrected dataset.")
+            description="key in h5py where data array is stored")
     # s2p registration settings
     do_registration = argschema.fields.Int(
             default=0,
-            description="0 skips registration, as we are doing our own.")
+            description=("0 skips registration (not well-documented). In "
+                         "Allen production case, we are providing motion-"
+                         "corrected videos, so we skip Suite2P's own "
+                         "registration."))
     # s2p cell detection settings
     roidetect = argschema.fields.Bool(
             default=True,
-            description="Whether or not to run ROI extraction.")
+            description=("Whether or not to run ROI extraction. This is the "
+                         "main role of Suite2P in Allen production."))
     sparse_mode = argschema.fields.Bool(
             default=True,
-            description="Whether or not to run 'sparse mode'.")
+            description=("From conversation with authors, True is the "
+                         "preferred mode."))
     diameter = argschema.fields.Int(
             default=12,
             description=("If not sparse_mode, use diameter (presumably "
@@ -44,52 +54,68 @@ class Suite2PWrapperSchema(argschema.ArgSchema):
     spatial_scale = argschema.fields.Int(
             default=0,
             description=("0: multi-scale; 1: 6 pix; 2: 12 pix;"
-                         "3: 24 pix; 4: 48 pix"))
+                         "3: 24 pix; 4: 48 pix. From conversation with "
+                         "authors, 0 is the preferred mode."))
     connected = argschema.fields.Bool(
             default=True,
-            description="Whether to keep ROIs fully connected.")
+            description=("Whether to keep ROIs fully connected. This is "
+                         "Suite2P default."))
     nbinned = argschema.fields.Int(
             required=False,
-            description="Max num of binned frames for cell detection.")
+            description=("Max num of binned frames for cell detection. Below "
+                         "is an Allen-specific parameter `bin_size` from "
+                         "which this setting is derived, if not provided."))
     max_iterations = argschema.fields.Int(
             default=20,
-            description="Max num iterations to detect cells.")
+            description="Max num iterations to detect cells. Suite2P default")
     threshold_scaling = argschema.fields.Float(
             default=0.75,
             description=("Adjust automatically determined threshold by this "
-                         "scalar multiplier"))
+                         "scalar multiplier. From an extensive validation "
+                         "effort, we found that 0.75 segmented expected cells "
+                         "but higher values tended to miss some. A fair "
+                         "number of non-cells also are segmented, and need "
+                         "to be filtered by a classifier."))
     max_overlap = argschema.fields.Float(
             default=0.75,
             description=("Cells with more overlap than this get removed "
-                         "during triage, before refinement"))
+                         "during triage, before refinement. Suite2P default."))
     high_pass = argschema.fields.Int(
             default=100,
             description=("Running mean subtraction with window of "
-                         "size 'high_pass'"))
+                         "size 'high_pass'. Suite2P default."))
     smooth_masks = argschema.fields.Bool(
             default=True,
             description=("Whether to smooth masks in final pass of cell "
                          "detection. Especially useful if one is in a high "
-                         "noise regime."))
+                         "noise regime. It appears this is only applicable "
+                         "with sparse_mode=False, which is not our default."))
     # s2p ROI extraction parameters
     inner_neuropil_radius = argschema.fields.Int(
             default=2,
             description=("Number of pixels to keep between ROI and neuropil "
-                         "donut."))
+                         "donut. "
+                         "Allen production will run it's own neuropil "
+                         "subtraction routine."))
     min_neuropil_pixels = argschema.fields.Int(
             default=350,
-            description=("Minimum number of pixels in the neuropil."))
+            description=("Minimum number of pixels in the neuropil."
+                         "Allen production will run it's own neuropil "
+                         "subtraction routine."))
     allow_overlap = argschema.fields.Bool(
             # NOTE: changing this from extraction validation and labeling
             # will be untangled downstream in demixing.
             default=True,
             description=("Pixels that are overlapping are thrown out (False) "
-                         "or added to both ROIs (True)"))
+                         "or added to both ROIs (True). Allen production will "
+                         "run a demixing step that should allow both ROIs "
+                         "to share pixels."))
     # Allen-specific options
     bin_size = argschema.fields.Int(
             required=False,
             description=("If nbinned not provided, will calculate nbinned as "
-                         "nframes / bin_size"))
+                         "nframes / bin_size. This allows consistent temporal "
+                         "downsampling across movies of different lengths."))
     output_dir = argschema.fields.OutputDir(
             required=True,
             description="for minimal and cleaner output, specifies output dir")
@@ -104,7 +130,8 @@ class Suite2PWrapperSchema(argschema.ArgSchema):
     timestamp = argschema.fields.Bool(
             default=True,
             description=("if true, will add a timestamp to the output file "
-                         "names"))
+                         "names. "
+                         "<basename>.<ext> -> <basename>_<timestamp>.<ext>"))
 
     @mm.post_load
     def check_args(self, data, **kwargs):

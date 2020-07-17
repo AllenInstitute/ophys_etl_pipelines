9d4ec537f2ebdafe33a20a15e74f5323df509334
isaak.willett@alleninstitute.org <isaak.willett@alleninstitute.org>
2020-07-17 15:16:36 -0700

    A function was added to get max motion correction
    A function was added to load max motion correction values from the rigid
    transform csv file created during motion correction workflow step. These
    values are used when writing ROIS to a file as these objects require max
    corrections in every cardinal direction as part of their data structure.

---
+++ src/ophys_etl/transforms/data_loaders.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+from collections import namedtuple
+
+from pathlib import Path
+from typing import Tuple
+
+motion_border = namedtuple('motion_border', 'left right up down')
+
+
+def get_max_correction_border(motion_correction_path: Path,
+                              maximum_shift: float = 30.0) -> motion_border:
+    """
+    Gets the maximum correction values in the cardinal directions from a
+    rigid motion transform file created during motion correction workflow
+    step.
+    Parameters
+    ----------
+    motion_correction_path: Path
+        The path to the csv file that contains the motion correction data.
+    maximum_shift: float
+        Maximum shift to allow when considering motion correction. Any
+        larger shifts are considered outliers.
+
+    Returns
+    -------
+    Tuple[float, float, float, float]
+        A tuple containing the maximum correction values found during motion
+        correction workflow step. Saved with the direction is the order
+        [Left, Right, Up, Down]
+    """
+    # load motion correction dataframe from file
+    if not motion_correction_path.exists():
+        raise FileNotFoundError("Motion Correction File Not Found")
+
+    # pass to private function for calculation
+    motion_correction_df = pd.read_csv(motion_correction_path)
+    max_motion_border = _get_max_correction_values(motion_correction_df,
+                                                   maximum_shift)
+    return max_motion_border
+
+
+def _get_max_correction_values(motion_corr_df: pd.DataFrame,
+                               max_shift: float = 30.0) -> motion_border:
+    """
+    Gets the max correction values in the cardinal directions from
+    a motion correction dataframe created during Motion Correction workflow
+    step.
+    Parameters
+    ----------
+    motion_corr_df: pd.DataFrame
+        The dataframe that contains the motion correction value calculated
+        during motion correction workflow step of ophys segmentation pipeline.
+    max_shift: float
+        Maximum shift to allow when considering motion correction. Any
+        larger shifts are considered outliers.
+
+    Returns
+    -------
+    Tuple[float, float, float, float]
+        A tuple containing the maximum correction values found during motion
+        correction workflow step. Saved with the direction is the order
+        [Left, Right, Up, Down] and also with names.
+
+    """
+    # validate if input columns match supported versions of motion correction
+    # files
+    input_column_set = set(motion_corr_df.columns)
+    required_columns = {'x', 'y'}
+    if not required_columns.intersection(input_column_set) == required_columns:
+        raise KeyError("Required columns to compute max correction not in "
+                       f"input dataset. Supplied columns: {input_column_set}, "
+                       f"need columns {required_columns}")
+    if max_shift <= 0:
+        raise ValueError(f"Max Shift input: {max_shift}. A positive input"
+                         f"input for max shift is required.")
+
+    # filter based out analomies based on maximum_shift
+    x_no_outliers = motion_corr_df["x"][(motion_corr_df["x"] >= -max_shift) &
+                                        (motion_corr_df["x"] <= max_shift)]
+    y_no_outliers = motion_corr_df["y"][(motion_corr_df["y"] >= -max_shift) &
+                                        (motion_corr_df["y"] <= max_shift)]
+    # calculate max border shifts
+    right_shift = -1 * x_no_outliers.min()
+    left_shift = x_no_outliers.max()
+    down_shift = -1 * y_no_outliers.min()
+    up_shift = y_no_outliers.max()
+
+    max_border = motion_border(left=left_shift, right=right_shift,
+                               up=up_shift, down=down_shift)
+
+    # check if all exist
+    if np.any(np.isnan(np.array(max_border))):
+        raise ValueError("One or more motion correction border directions "
+                         "was found to be Nan, max motion border found: "
+                         f"{max_border}, with max_shift {max_shift}")
+
+    return max_border
---
+++ tests/transforms/test_data_loaders.py
@@ -0,0 +1,97 @@
+import tempfile
+from pathlib import Path
+
+import pytest
+import numpy as np
+import pandas as pd
+
+from ophys_etl.transforms.data_loaders import (get_max_correction_border,
+                                               _get_max_correction_values,
+                                               motion_border)
+
+
+@pytest.mark.parametrize("motion_correction_data, expected_max_border, x_fail",
+                         [({'framenumber': [0, 1, 2, 3, 4, 5],
+                            'x': [-0.187, -0.555, -0.150,
+                                  -0.159, 0.047, 0.964],
+                            'y': [0.430, -1.096, -1.774,
+                                -2.773, -3.920, -4.633],
+                            'correlation': [70002.9, 61688.8,
+                                            48442.2, 39674.9,
+                                            25735.2, 13894.7],
+                            'kalman_x': [-0.001, -0.004,
+                                         -0.006, -0.007,
+                                         -0.007, 0.009],
+                            'kalman_y': [-0.002, -0.009,
+                                         -0.024, -0.055,
+                                         -0.108, -0.182]},
+                           motion_border(left=0.964, right=0.555, down=4.633,
+                                         up=0.430),
+                           False),
+                          ({}, (), True)])  # data doesn't matter file not written
+def test_get_max_correction_border_from_file(motion_correction_data,
+                                             expected_max_border,
+                                             x_fail):
+    motion_correction_df = pd.DataFrame.from_dict(motion_correction_data)
+    with tempfile.TemporaryDirectory() as tempdir:
+        temp_file_path = Path(tempdir) / 'test_rigid_motion_transform.csv'
+        if x_fail:
+            with pytest.raises(FileNotFoundError,
+                               match="Motion Correction File Not Found"):
+                get_max_correction_border(temp_file_path)
+        else:
+            motion_correction_df.to_csv(temp_file_path.as_posix(), index=True)
+            correction_border = get_max_correction_border(temp_file_path)
+            np.testing.assert_allclose(np.array(expected_max_border),
+                                       np.array(correction_border))
+
+
+@pytest.mark.parametrize("motion_correction_data, max_shift,"
+                         "expected_motion_border, x_fail, expected_error",
+                         [({'x': [None, None],
+                            'y': [0.430, 0.321]}, 30.0,
+                             None, True, ValueError),
+                          ({'x': [0.430, 0.321],
+                            'frame_number': [0, 1]}, 30.0,
+                             None, True, KeyError),
+                          ({'x': [31, -32],
+                            'y': [0.54, -0.17]}, 30.0,
+                             None, True, ValueError),
+                          ({'x': [15],
+                            'y': [12]}, 30.0,
+                           motion_border(left=15, right=-15, up=12, down=-12),
+                           False, None),
+                          ({'x': [-15],
+                            'y': [-12]}, 30.0,
+                           motion_border(left=-15, right=15, up=-12, down=12),
+                           False, None),
+                          ({'x': [15],
+                            'y': [12]}, -1,
+                           None, True, ValueError),
+                          ({'x': [15, 12, 0.5, 0.67, -2],
+                            'y': [7, 15, 0.56, -2.3, 4]}, 30.0,
+                           motion_border(left=15, right=2, up=15, down=2.3),
+                           False, None)])
+def test_get_max_correction_border(motion_correction_data, max_shift,
+                                   expected_motion_border, x_fail,
+                                   expected_error):
+    """
+    Test Cases:
+    1. No values in one of required column
+    2. Missing required column
+    3. One column no values abs less than max shift
+    4. One value per column
+    5. One negative value per column
+    6. Negative max shift
+    7. Standard case with positive and negative values
+    """
+    motion_correction_df = pd.DataFrame.from_dict(motion_correction_data)
+    if x_fail:
+        with pytest.raises(expected_error):
+            _get_max_correction_values(motion_correction_df,
+                                       max_shift=max_shift)
+    else:
+        calculated_border = _get_max_correction_values(motion_correction_df,
+                                                       max_shift=max_shift)
+        np.testing.assert_allclose(np.array(expected_motion_border),
+                                   np.array(calculated_border))

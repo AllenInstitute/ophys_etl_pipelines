cfafba1456e34c763e697d6bb899485486ac4b7c
Dan <danielk@alleninstitute.org>
2020-08-04 17:16:14 -0700

    adds chained transform pipeline for single LIMS call

---
+++ src/ophys_etl/pipelines/segment_binarize_pipeline.py
@@ -0,0 +1,53 @@
+import argschema
+from ophys_etl.transforms.suite2p_wrapper import Suite2PWrapper
+from ophys_etl.transforms.convert_rois import BinarizerAndROICreator
+import tempfile
+import pathlib
+import json
+
+
+class SegmentBinarizeSchema(argschema.ArgSchema):
+    motion_corrected_video = argschema.fields.InputFile(
+        required=True,
+        description="hdf5 motion-correced video")
+    motion_correction_values = argschema.fields.InputFile(
+        required=True,
+        description="motion correction values in a csv")
+
+
+class SegmentBinarize(argschema.ArgSchemaParser):
+    default_schema = SegmentBinarizeSchema
+
+    def run(self):
+        tmpdir = tempfile.TemporaryDirectory()
+        Suite2P_output = pathlib.Path(tmpdir.name) / "Suite2P_output.json"
+
+        # segment with Suite2P, almost all default args
+        segment_args = {
+                "h5py": self.args['motion_corrected_video'],
+                "output_dir": tmpdir.name,
+                "output_json": str(Suite2P_output),
+                "retain_files": ["stat.npy"],
+                "bin_size": 115
+                }
+        segment = Suite2PWrapper(input_data=segment_args, args=[])
+        segment.run()
+
+        # binarize and output LIMS format
+        with open(Suite2P_output, "r") as f:
+            stat = json.load(f)['output_files']['stat.npy']
+
+        binarize_args = {"suite2p_stat_path": stat}
+
+        for k in ['output_json', 'motion_corrected_video',
+                  'motion_correction_values']:
+            binarize_args[k] = self.args[k]
+
+        binarize = BinarizerAndROICreator(input_data=binarize_args, args=[])
+        binarize.binarize_and_create()
+        tmpdir.cleanup()
+
+
+if __name__ == "__main__":  # pragma: nocover
+    sbmod = SegmentBinarize()
+    sbmod.run()
---
+++ tests/pipelines/test_segment_binarize_pipeline.py
@@ -0,0 +1,76 @@
+import h5py
+import numpy as np
+from unittest.mock import patch, Mock
+import pathlib
+import argschema
+import json
+import ophys_etl.transforms.convert_rois as crois
+
+import sys
+sys.modules['suite2p'] = Mock()
+import ophys_etl.transforms.suite2p_wrapper as s2pw  # noqa
+import ophys_etl.pipelines.segment_binarize_pipeline as sbpipe  # noqa
+
+
+class MockSuite2PWrapper(argschema.ArgSchemaParser):
+    default_schema = s2pw.Suite2PWrapperSchema
+    default_output_schema = s2pw.Suite2PWrapperOutputSchema
+
+    def run(self):
+        stat = pathlib.Path(self.args['output_dir']) / "stat.npy"
+        with open(stat, "w") as f:
+            f.write("content")
+        outj = {
+                'output_files': {
+                    'stat.npy': str(stat)
+                    }
+                }
+        self.output(outj)
+
+
+class MockOutputSchema(argschema.schemas.DefaultSchema):
+    some_output = argschema.fields.Str(
+        required=True)
+
+
+class MockBinarizer(argschema.ArgSchemaParser):
+    default_schema = crois.BinarizeAndCreateROIsInputSchema
+    default_output_schema = MockOutputSchema
+
+    def binarize_and_create(self):
+        self.output({'some_output': 'junk'})
+
+
+@patch(
+        'ophys_etl.pipelines.segment_binarize_pipeline.Suite2PWrapper',
+        MockSuite2PWrapper)
+@patch(
+        'ophys_etl.pipelines.segment_binarize_pipeline.BinarizerAndROICreator',
+        MockBinarizer)
+def test_segment_binarize_pipeline(tmp_path):
+    """tests that satisfying the pipeline schema satisfies the
+    internal transform schema.
+    """
+    h5path = tmp_path / "mc_video.h5"
+    with h5py.File(str(h5path), "w") as f:
+        f.create_dataset("data", data=np.zeros((20, 100, 100)))
+
+    mcvalues_path = tmp_path / "mc_values.csv"
+    with open(mcvalues_path, "w") as f:
+        f.write("content")
+
+    outj_path = tmp_path / "output.json"
+
+    args = {
+            "motion_corrected_video": str(h5path),
+            "motion_correction_values": str(mcvalues_path),
+            "output_json": str(outj_path)
+            }
+
+    sbp = sbpipe.SegmentBinarize(input_data=args, args=[])
+    sbp.run()
+
+    with open(outj_path, "r") as f:
+        outj = json.load(f)
+
+    assert outj == {'some_output': 'junk'}

867b395070a1c42583bd925aec742e406ad25ebd
Dan <danielk@alleninstitute.org>
2020-08-20 10:36:21 -0700

    adds empty and singleton list to aspect filter

--- tests/filters/test_filters.py
+++ tests/filters/test_filters.py
@@ -24,3 +24,17 @@ def test_filter_rois_by_aspect_ratio(threshold):
     assert len(clarger) == len(filtered)
     for i, j in zip(clarger, filtered):
         np.testing.assert_array_equal(i.toarray(), j.toarray())
+
+
+def test_filter_rois_by_aspect_ratio_edge_cases():
+    threshold = 0.2
+    filtered = filter_by_aspect_ratio([], threshold)
+    assert filtered == []
+    
+    csmaller, clarger = coos_by_aspect(threshold)
+
+    filtered = filter_by_aspect_ratio(csmaller[0:1], threshold)
+    assert filtered == []
+
+    filtered = filter_by_aspect_ratio(clarger[0:1], threshold)
+    assert filtered == clarger[0:1]

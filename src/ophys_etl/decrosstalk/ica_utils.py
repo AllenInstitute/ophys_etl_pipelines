0c2fc1f3420384bd673ec05814b8f9edb56a6eca
danielsf <scott.daniel@alleninstitute.org>
2021-01-08 17:21:18 -0800

    ica_utils.py -- swap columns of mixing matrix
    If it is determined that the signal and crosstalk channel returned
    by FastICA need to be swapped, then the columns of the mixing matrix
    also need to be swapped so that
    
    np.dot(mixing_matrix, [signal, crosstalk])
    
    still returns the input data.
    
    Also: add 'verbose' as kwarg to run_ica so that unit test can
    make sure that it has tested the case where the columns were swapped

--- src/ophys_etl/decrosstalk/ica_utils.py
+++ src/ophys_etl/decrosstalk/ica_utils.py
@@ -71,7 +71,33 @@ def fix_source_assignment(ica_input, ica_output):
     return ica_output_corrected, corrs, swapped_flag
 
 
-def run_ica(ica_input, iters, seed):
+def run_ica(ica_input, iters, seed, verbose=False):
+    """
+    ica_input -- an NxM numpy array; in the context of the decrosstalk
+    problem, N=the number of timesteps; M=2 (first column is signal;
+    second is crosstalk)
+
+    iters -- an int; the number of iterative loops to try to go through
+    to get the off-diagonal elements of the mixing matrix < 0.3
+
+    seed -- an int; the seed of the random number generator that will
+    be fed to sklearn.decompositions.FastICA
+
+    verbose -- if True, also returns a flag indicating whether or not
+    ICA output signals had to be swapped (just used for testing)
+
+    Returns
+    -------
+    ica_output -- and MxN numpy array; ica_output[0,:] is the unmixed signal,
+    ica_output[1,:] is the unmixed crosstalk (in the context of the decrosstalk
+    problem)
+
+    mixing -- the mixing matrix that gets from ica_input to ica_output
+    np.dot(mixing, ica_output).transpose() will restore ica_input
+
+    roi_demixed -- a boolean indicating whether or not the iteration to get
+    the off-diagonal elements of the mixing matrix < 0.3 actually worked
+    """
     # Whiten observations
     Ow, W, m = whiten_data(ica_input)
     alpha = 1
@@ -101,6 +127,15 @@ def run_ica(ica_input, iters, seed):
 
     Sos = np.dot(ica_input, np.linalg.inv(mixing).T)  # recovering outputs using new mixing matrix
 
-    ica_output, corrs, _ = fix_source_assignment(ica_input.T, Sos.T) # fixing source assignment ambiguity
+    ica_output, corrs, swapped = fix_source_assignment(ica_input.T, Sos.T) # fixing source assignment ambiguity
+
+    if swapped:
+        new_mixing = np.zeros((2,2), dtype=float)
+        new_mixing[:,1] = mixing[:,0]
+        new_mixing[:,0] = mixing[:,1]
+        mixing = new_mixing
+
+    if verbose:
+        return ica_output, mixing, roi_demixed, swapped
 
     return ica_output, mixing, roi_demixed

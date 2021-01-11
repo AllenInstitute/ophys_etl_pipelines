e789d8b4991e177e8c27f33ae7b6e30e043c3bc7
danielsf <scott.daniel@alleninstitute.org>
2021-01-11 10:49:50 -0800

    decrosstalk_utils.py -- add validate_cell_crosstalk and test

--- src/ophys_etl/decrosstalk/decrosstalk_utils.py
+++ src/ophys_etl/decrosstalk/decrosstalk_utils.py
@@ -27,3 +27,31 @@ def find_independent_events(signal_events, crosstalk_events, window=2):
     valid_signal_events = np.where(np.logical_not(np.isin(signal_events['events'], blurred_crosstalk)))
     return {'trace': signal_events['trace'][valid_signal_events],
             'events': signal_events['events'][valid_signal_events]}
+
+
+def validate_cell_crosstalk(signal_events, crosstalk_events, window=2):
+    """
+    Determine if an ROI is a valid cell or a ghost based on the events detected in the
+    signal and crosstalk channels
+
+    Args:
+        signal_events: a dict
+            signal_events['trace'] is an array of the trace flux values of the signal channel
+            signal_events['events'] is an array of the timestamp indices of the signal channel
+        crosstalk_events: a dict (same structure as signal_events)
+        window (int): the amount of blurring to use in find_independent_events (default=2)
+
+    Returns:
+        is_valid_roi : a boolean that is true if there are any independent events in the signal channel
+
+        independent_events: a dict of events that were in signal_events, but not crosstalk_events +/- window
+            independent_events['trace'] is an array of the trace flux values
+            indpendent_events['events'] is an array of the timestamp indices
+
+    """
+
+    independent_events = find_independent_events(signal_events, crosstalk_events, window=window)
+    is_valid_roi = False
+    if len(independent_events['events'])>0:
+        is_valid_roi = True
+    return is_valid_roi, independent_events

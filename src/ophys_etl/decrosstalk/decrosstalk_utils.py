91194a992bbea03eb472e021b2431acceea7de73
danielsf <scott.daniel@alleninstitute.org>
2021-01-15 12:58:04 -0800

    decrosstalk_utils.py -- add decrosstalk_utils.py
    This is module contains high level processing methods that are meant
    to be run on the outputs of methods in ica_utils.py and active_traces.py

---
+++ src/ophys_etl/decrosstalk/decrosstalk_utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def find_independent_events(signal_events, crosstalk_events, window=2):
+    ''' Calculate independent events between signal_events and crosstalk_events.
+
+    The algorithm uses window to extend the range of event matches, such that
+    if an event happens at time t in the signal and time t+window in the crosstalk,
+    they are *not* considered independent events. If window=0, then any events
+    that are not exact matches (i.e. occurring at the same time point) will be considered
+    independent events.
+
+    Args:
+        signal_events: a dict
+            signal_events['trace'] is an array of the trace flux values of the signal channel
+            signal_events['events'] is an array of the timestamp indices of the signal channel
+        crosstalk_events: a dict (same structure as signal_events)
+        window (int): the amount of blurring to use (default=2)
+
+    Returns:
+        independent_events: a dict of events that were in signal_events, but not crosstalk_events +/- window
+            independent_events['trace'] is an array of the trace flux values
+            indpendent_events['events'] is an array of the timestamp indices
+    '''
+    blurred_crosstalk = np.unique(np.concatenate([crosstalk_events['events']+ii
+                                                  for ii in np.arange(-window, window+1)]))
+
+    valid_signal_events = np.where(np.logical_not(np.isin(signal_events['events'], blurred_crosstalk)))
+    return {'trace': signal_events['trace'][valid_signal_events],
+            'events': signal_events['events'][valid_signal_events]}

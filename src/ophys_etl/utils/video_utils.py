462c7c80cd15cb498db834c7d97d4d3862b87392
Nicholas Mei <nicholas.mei@alleninstitute.org>
2021-01-27 23:56:19 -0800

    Resolve runaway memory usage when converting video to webm
    The previous strategy of splitting the video array into time chunks
    and using parallel processing appears to be the cause of blown up
    memory usage.
    
    This commit resolves a runaway memory usage issue when trying to
    convert motion corrected and non-motion corrected microscopy
    videos to a portable *.webm format.
    
    The solution was to just directly feed video frames to the ffmpeg
    writer instead of parallelizing the frame writing process.

--- src/ophys_etl/utils/video_utils.py
+++ src/ophys_etl/utils/video_utils.py
@@ -1,10 +1,7 @@
 import h5py
-import tempfile
-import multiprocessing
-import subprocess
 import numpy as np
 import imageio_ffmpeg as mpg
-from typing import Union, List
+from typing import Union
 from pathlib import Path
 from ophys_etl.utils.array_utils import downsample_array
 
@@ -49,86 +46,6 @@ def downsample_h5_video(
     return video_out
 
 
-def transform_to_webm(video: np.ndarray, output_path: str,
-                      fps: float, ncpu: int, bitrate: str = "0",
-                      crf: int = 20) -> str:
-    """Function to transform 2p gray scale video into a webm
-    video using imageio_ffmpeg.
-
-    Parameters
-    ----------
-    video : np.ndarray
-        Video to be transformed with shape (time, row, col)
-    output_path : str
-        Output path for the transformed video
-    fps : float
-        Desired frames per second (fps) of the output video
-    ncpu : int
-        Degree of parallelization desired for encoding. Video will be
-        split into 'ncpu' parts and each part will be encoded in parallel.
-    bitrate : str, optional
-        Desired bitrate of output, by default "0". The default *MUST*
-        be zero in order to encode in constant quality mode.
-    crf : int, optional
-        Desired perceptual quality of output, by default 20. Value can
-        be from 0 - 63. Lower values mean better quality.
-
-    Returns
-    -------
-    str
-        Output path of the encoded video
-    """
-
-    split_video = np.array_split(video, ncpu)
-    split_output_paths = [tempfile.NamedTemporaryFile(suffix=f"_{i}.webm")
-                          for i in range(ncpu)]
-
-    mp_pool_args = [(vid, outpath.name, fps, bitrate, crf)
-                    for vid, outpath in zip(split_video, split_output_paths)]
-
-    with multiprocessing.Pool(ncpu) as pool:
-        encode_results = pool.starmap(encode_video, mp_pool_args)
-
-    concat_output = concat_videos(encode_results, output_path)
-
-    for sop in split_output_paths:
-        sop.close()
-
-    return concat_output
-
-
-def concat_videos(video_paths: List[str], output_path: str) -> str:
-    """Use ffmpeg to concatenate a list of videos (with the same encoding)
-    into a single video.
-
-    Parameters
-    ----------
-    video_paths : List[str]
-        A list of paths (str) for videos that should be concatenated together
-        Order of paths matters!
-    output_path : str
-        The desired output path for the concatenated video
-
-    Returns
-    -------
-    str
-        Path of concatenated video
-    """
-
-    with tempfile.NamedTemporaryFile(suffix=".txt") as concat_list_file:
-        with open(concat_list_file.name, 'w') as fp:
-            for path in video_paths:
-                fp.write(f"file '{path}'\n")
-
-        # See: https://trac.ffmpeg.org/wiki/Concatenate
-        ffmpeg_concat_cmd = [mpg.get_ffmpeg_exe(), '-y', '-f', 'concat',
-                             '-safe', '0', '-i', concat_list_file.name,
-                             '-c', 'copy', output_path]
-        subprocess.run(ffmpeg_concat_cmd)
-
-    return output_path
-
-
 def encode_video(video: np.ndarray, output_path: str,
                  fps: float, bitrate: str = "0", crf: int = 20) -> str:
     """Encode a video with vp9 codec via imageio-ffmpeg
@@ -141,8 +58,6 @@ def encode_video(video: np.ndarray, output_path: str,
         Desired output path for encoded video
     fps : float
         Desired frame rate for encoded video
-    pix_fmt_out : str
-        Desired pixel format for output encoded video, by default "yuv420p"
     bitrate : str, optional
         Desired bitrate of output, by default "0". The default *MUST*
         be zero in order to encode in constant quality mode. Other values

a1e3aeef03dabe246abc4b38be3e7e911e63d5ac
Dan <danielk@alleninstitute.org>
2021-02-01 07:07:59 -0800

    clips x & y offsets relative to a median-filtered trend and rewrites affected frames in the motion-corrected movie

--- tests/pipelines/test_suite2p_registration.py
+++ tests/pipelines/test_suite2p_registration.py
@@ -11,6 +11,7 @@ import pytest
 import tifffile
 
 sys.modules['suite2p'] = Mock()
+sys.modules['suite2p.registration.rigid'] = Mock()
 import ophys_etl.pipelines.suite2p_registration as s2preg  # noqa
 import ophys_etl.transforms.suite2p_wrapper as s2pw  # noqa
 
@@ -46,6 +47,41 @@ class MockSuite2PWrapper(argschema.ArgSchemaParser):
                     }
                 }
         self.output(outj)
+
+
+@pytest.mark.suite2p_only
+@pytest.mark.parametrize(
+        ("excess_indices", "deltas", "thresh", "expected_indices"), [
+            ([], [], 10, []),
+            ([123, 567], [20, 20], 10, [123, 567]),
+            ([123, 567], [8, 20], 10, [567]),
+            ([123, 567], [-20, -20], 10, [123, 567]),
+            ([123, 567], [-8, -20], 10, [567]),
+            ([123, 567, 1234, 5678], [-20, -8, 8, 20], 10, [123, 5678])
+            ])
+def test_identify_and_clip_outliers(excess_indices, deltas,
+                                    thresh, expected_indices):
+    frame_index = np.arange(10000)
+    # long-range drifts
+    baseline = 20.0 * np.cos(2.0 * np.pi * frame_index / 500)
+    additional = np.zeros_like(baseline)
+    for index, delta in zip(excess_indices, deltas):
+        additional[index] += delta
+
+    data, indices = s2preg.identify_and_clip_outliers(
+            baseline + additional,
+            10,
+            thresh)
+
+    # check that the outliers were identified
+    assert set(indices) == set(expected_indices)
+    # check that the outlier values were clipped to within
+    # the threshold of the underlying trend data
+    # with a small delta value because the median-filtered data
+    # is not quite ever the same as the baseline data
+    deltas = np.abs(data[expected_indices] - baseline[expected_indices])
+    small_delta = 1.0
+    assert np.all(deltas < thresh + small_delta)
 
 
 @pytest.mark.suite2p_only

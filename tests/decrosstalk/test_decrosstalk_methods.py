7bc8e20b302c7e6e61246636d24db3b56cd5421a
danielsf <scott.daniel@alleninstitute.org>
2021-02-01 13:50:24 -0800

    fix test to refer to mean, rather than median; add edge cases

--- tests/decrosstalk/test_decrosstalk_methods.py
+++ tests/decrosstalk/test_decrosstalk_methods.py
@@ -3,12 +3,12 @@ import ophys_etl.decrosstalk.decrosstalk_types as dc_types
 import ophys_etl.decrosstalk.decrosstalk as decrosstalk
 
 
-def test_rolling_median_and_std():
+def test_rolling_mean_and_std():
 
     rng = np.random.RandomState(11123)
     data = rng.random_sample(100)
     mask = np.ones(100, dtype=bool)
-    med, std = decrosstalk._centered_rolling_mean(data, mask, window=10)
+    mean, std = decrosstalk._centered_rolling_mean(data, mask, window=10)
     for ii in range(100):
         i0 = ii-5
         if i0 < 0:
@@ -19,11 +19,11 @@ def test_rolling_median_and_std():
             i0 = 90
         m = np.mean(data[i0:i1])
         s = np.std(data[i0:i1], ddof=1)
-        assert abs(m-med[ii]) < 1.0e-10
+        assert abs(m-mean[ii]) < 1.0e-10
         assert abs(s-std[ii]) < 1.0e-10
 
     mask[0:100:2] = False
-    med, std = decrosstalk._centered_rolling_mean(data, mask, window=10)
+    mean, std = decrosstalk._centered_rolling_mean(data, mask, window=10)
     for ii in range(100):
         i0 = ii-5
         if i0 < 0:
@@ -39,8 +39,59 @@ def test_rolling_median_and_std():
             m = np.mean(data[i0:i1:2])
             s = np.std(data[i0:i1:2], ddof=1)
 
-        assert abs(m-med[ii]) < 1.0e-10
+        assert abs(m-mean[ii]) < 1.0e-10
         assert abs(s-std[ii]) < 1.0e-10
+
+    mask = np.ones(100, dtype=bool)
+    idx = np.unique(rng.randint(0,100,20))
+    mask[idx] = False
+
+    mean, std = decrosstalk._centered_rolling_mean(data, mask, window=10)
+    for ii in range(100):
+        i0 = ii-5
+        if i0 < 0:
+            i0 = 0
+        i1 = i0+10
+        if i1 > 100:
+            i1 = 100
+            i0 = 90
+        d = data[i0:i1][mask[i0:i1]]
+        m = np.mean(d)
+        s = np.std(d, ddof=1)
+
+        assert abs(m-mean[ii]) < 1.0e-10
+        assert abs(s-std[ii]) < 1.0e-10
+
+    # test case where stretch of `False` in mask is longer than window
+    edge_case_exercised = False
+    mask[50:75] = False
+    mean, std = decrosstalk._centered_rolling_mean(data, mask, window=10)
+    for ii in range(100):
+        i0 = ii-5
+        if i0 < 0:
+            i0 = 0
+        i1 = i0+10
+        if i1 > 100:
+            i1 = 100
+            i0 = 90
+        sub_mask = mask[i0:i1]
+        d = data[i0:i1][sub_mask]
+
+        if sub_mask.sum() < 2:
+            edge_case_exercised = True
+            if sub_mask.sum() == 0:
+                assert np.isnan(mean[ii])
+            else:
+                assert abs(mean[ii] - np.mean(d)) < 1.0e-10
+            assert np.isnan(std[ii])
+        else:
+            m = np.mean(d)
+            s = np.std(d, ddof=1)
+            assert abs(m-mean[ii]) < 1.0e-10
+            assert abs(s-std[ii]) < 1.0e-10
+
+    assert edge_case_exercised
+
 
 
 def test_clean_negative_traces():

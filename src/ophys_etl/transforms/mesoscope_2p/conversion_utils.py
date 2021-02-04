39de0739617eb78e5c8c40139730560a2c7cf83f
danielsf <scott.daniel@alleninstitute.org>
2021-02-03 17:57:42 -0800

    make volume_to_h5 able to take either a path or a file handle

--- src/ophys_etl/transforms/mesoscope_2p/conversion_utils.py
+++ src/ophys_etl/transforms/mesoscope_2p/conversion_utils.py
@@ -21,21 +21,47 @@ def dump_dict_as_attrs(h5fp, container_name, data):
     return container
 
 
-def volume_to_h5(h5fp, volume, dset_name="data", page_block_size=None,
+def _volume_to_h5(h5fp, volume, dset_name="data", page_block_size=None,
                  **h5_opts):
-    if isinstance(h5fp, str):
-        f = h5py.File(h5fp, "w")
+    """
+    Actually do the work of writing the TIFF volume to the HDF5 file;
+    h5fp is an h5py.File handle
+    """
 
     if page_block_size is None:
-        dset = f.create_dataset(dset_name, data=volume[:],
-                                **h5_opts)
+        dset = h5fp.create_dataset(dset_name, data=volume[:],
+                                   **h5_opts)
     else:
-        dset = f.create_dataset(dset_name, volume.shape,
-                                dtype=volume.dtype, **h5_opts)
+        dset = h5fp.create_dataset(dset_name, volume.shape,
+                                  dtype=volume.dtype, **h5_opts)
         i = 0
         while i < volume.shape[0]:
             dset[i:i+page_block_size, :, :] = volume[i:i+page_block_size]
             i += page_block_size
+
+
+def volume_to_h5(h5fp, volume, dset_name="data", page_block_size=None,
+                 **h5_opts):
+    """
+    Write a tiff volume to an HDF5 file.
+
+    h5fp can either be a string indicating the path to the HDF5 file
+    or an hf5py.File handle
+    """
+
+    if isinstance(h5fp, str):
+        with h5py.File(h5fp, 'w') as out_file:
+            _volume_to_h5(out_file, volume, dset_name=dset_name,
+                          page_block_size=page_block_size,
+                          **h5_opts)
+    elif isinstance(h5fp, h5py.File):
+        _volume_to_h5(h5fp, volume, dset_name=dset_name,
+                      page_block_size=page_block_size,
+                      **h5_opts)
+    else:
+        raise ValueError("h5fp passed to volume_to_h5 "
+                         "must be either a str or an h5py.File; "
+                         "you passed %s" % (str(type(h5fp))))
 
 
 def volume_to_tif(filename, volume, projection_func=None):

6ab357261e265f68864cfd64746f3484040350ba
Dan <danielk@alleninstitute.org>
2021-03-09 15:40:19 -0800

    adds schema and light refactor of sine_dewarp

--- src/ophys_etl/modules/sine_dewarp/__main__.py
+++ src/ophys_etl/modules/sine_dewarp/__main__.py
@@ -1,152 +1,77 @@
-import argparse
+import argschema
 import functools
-import logging
 import math
-import multiprocessing
 import time
 import h5py
-import json
 import numpy as np
+from multiprocessing import Pool
 
 from ophys_etl.modules.sine_dewarp import utils
+from ophys_etl.modules.sine_dewarp.schemas import SineDewarpInputSchema
 
-valid_nr_methods = [0, 1, 2, 3]
 
+class SineDewarp(argschema.ArgSchemaParser):
+    default_schema = SineDewarpInputSchema
 
-def run_dewarping(FOVwidth: int,
-                  noise_reduction: int,
-                  threads: int,
-                  input_file: str,
-                  input_dataset: str,
-                  output_file: str,
-                  output_dataset: str,
-                  aL: float,
-                  aR: float,
-                  bL: float,
-                  bR: float) -> None:
-    """
-    Dewarp the movie frame by frame, utilizing multiprocessing (along with
-    manual chunking of the movie beforehand) to increase efficiency.
+    def run(self):
+        self.logger.name = type(self).__name__
 
-    Parameters
-    ----------
-    FOVwidth: int
-        Field of View width. Will be the width of the output image, unless 0,
-        then it will be the width of the input image.
-    noise_reduction: int
-        The noise reduction method that will be used in dewarping.
-    threads: int
-        The number of worker threads to use in multiprocessing.
-    input_file: str
-        File path where the input h5 is saved.
-    input_dataset: str
-        The name of the dataset representing the movie within the
-        input h5 file.
-    output_file: str
-        File path where the h5 containing the dewarped video will be saved.
-    output_dataset: str,
-        The name of the dataset representing the movie within the
-        output h5 file.
-    aL: float
-        How far into the image (from the left side inward, in pixels) the
-        warping occurs. This is specific to the experiment, and is known
-        at the time of data collection.
-    aR: float
-        How far into the image (from the right side inward, in pixels) the
-        warping occurs. This is specific to the experiment, and is known
-        at the time of data collection.
-    bL: float
-        Roughly, a measurement of how strong the warping effect was on
-        the left side for the given experiment.
-    bR: float
-        Roughly, a measurement of how strong the warping effect was on
-        the right side for the given experiment.
-    Returns
-    -------
-    """
+        # read the movie into memory
+        with h5py.File(self.args["input_h5"], "r") as input_file:
+            movie = input_file['data'][()]
+        movie_shape = movie.shape
+        movie_dtype = movie.dtype
+        T, y_orig, x_orig = movie.shape
+        self.logger.info(f"read {self.args['input_h5']} with shape "
+                         f"{movie_shape}")
 
-    # Get statistics about the movie
-    input_h5_file = h5py.File(input_file, 'r')
-    movie = input_h5_file[input_dataset]
-
-    movie_shape = movie.shape
-    movie_dtype = movie.dtype
-    T, y_orig, x_orig = movie.shape
-
-    xtable = utils.create_xtable(movie, aL, aR, bL, bR, noise_reduction)
-
-    utils.make_output_file(output_file, output_dataset, xtable,
-                           FOVwidth, movie_shape, movie_dtype)
-
-    start_time = time.time()
-    with multiprocessing.Pool(threads) as pool, \
-         h5py.File(output_file, "a") as f:
+        # prepare for writing the output movie
+        xtable = utils.create_xtable(movie,
+                                     self.args["aL"],
+                                     self.args["aR"],
+                                     self.args["bL"],
+                                     self.args["bR"],
+                                     self.args["noise_reduction"])
+        self.logger.info("created xtable")
+        utils.make_output_file(self.args["output_h5"],
+                               "data",
+                               xtable,
+                               self.args["FOV_width"],
+                               movie_shape,
+                               movie_dtype)
 
         fn = functools.partial(
-            utils.xdewarp,
-            FOVwidth=FOVwidth,
-            xtable=xtable,
-            noise_reduction=noise_reduction
-        )
+                utils.xdewarp,
+                FOVwidth=self.args["FOV_width"],
+                xtable=xtable,
+                noise_reduction=self.args["noise_reduction"])
 
-        # Chunk the movie up to prevent an error caused by multiprocessing
-        # returning too much data. Use math.ceil in case there are fewer
-        # than chunk_size frame, then we just make one chunk.
-        chunk_size = 1000
-        movie_chunks = np.array_split(
-            movie, math.ceil(T / chunk_size), axis=0
-        )
+        # write the output movie
+        start_time = time.time()
+        with Pool(self.args["n_parallel_workers"]) as pool, \
+                h5py.File(self.args["output_h5"], "a") as f:
+            # Chunk the movie up to prevent an error caused by multiprocessing
+            # returning too much data. Use math.ceil in case there are fewer
+            # than chunk_size frame, then we just make one chunk.
+            chunk_size = 1000
+            movie_chunks = np.array_split(
+                movie, math.ceil(T / chunk_size), axis=0
+            )
 
-        # Use multiprocessing to dewarp one chunk of the movie at a time
-        dewarped_so_far = 0
-        for movie_chunk in movie_chunks:
-            for frame_num, frame in enumerate(pool.map(fn, movie_chunk)):
-                f[output_dataset][dewarped_so_far + frame_num, :, :] = frame
+            # Use multiprocessing to dewarp one chunk of the movie at a time
+            dewarped_so_far = 0
+            for movie_chunk in movie_chunks:
+                for frame_num, frame in enumerate(pool.map(fn, movie_chunk)):
+                    f["data"][dewarped_so_far + frame_num, :, :] = \
+                            frame
+                dewarped_so_far = dewarped_so_far + len(movie_chunk)
 
-            dewarped_so_far = dewarped_so_far + len(movie_chunk)
+        end_time = time.time()
+        self.logger.info(f"Elapsed time (s): {end_time - start_time}")
 
-    input_h5_file.close()
-
-    end_time = time.time()
-    logging.debug(f"Elapsed time (s): {end_time - start_time}")
+        self.output(dict())
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_json')
-    parser.add_argument('output_json')
-    parser.add_argument('--log_level', default=logging.DEBUG)
-    parser.add_argument('--threads', default=4, type=int)
-    parser.add_argument('--FOVwidth', default=0, type=int)
-    parser.add_argument('--noise_reduction', default=0, type=int)
-    args = parser.parse_args()
-
-    logging.getLogger().setLevel(args.log_level)
-
-    if args.noise_reduction not in valid_nr_methods:
-        raise(
-            f"{args.noise_reduction} is not a valid noise "
-            f"reduction option. Must be one of {valid_nr_methods}. "
-            f"Using default value noise_reduction = 0.\n",
-            UserWarning
-        )
-    else:
-        logging.debug(f"noise_reduction: {args.noise_reduction}")
-
-    with open(args.input_json, 'r') as json_file:
-        input_data = json.load(json_file)
-    input_h5, output_h5, aL, aR, bL, bR = utils.parse_input(input_data)
-
-    run_dewarping(
-        FOVwidth=args.FOVwidth,
-        noise_reduction=args.noise_reduction,
-        threads=args.threads,
-        input_file=input_h5,
-        input_dataset="data",
-        output_file=output_h5,
-        output_dataset="data",
-        aL=aL, aR=aR, bL=bL, bR=bR
-    )
-
-    with open(args.output_json, 'w') as outfile:
-        json.dump({}, outfile)
+    smod = SineDewarp()
+    smod.run()
---
+++ tests/modules/sine_dewarp/test_sine_dewarp.py
@@ -0,0 +1,39 @@
+import pytest
+import h5py
+import numpy as np
+from pathlib import Path
+
+from ophys_etl.modules.sine_dewarp.__main__ import SineDewarp
+
+
+@pytest.fixture
+def input_h5(tmpdir):
+    fname = tmpdir / "input.h5"
+    with h5py.File(fname, "w") as f:
+        f.create_dataset("data",
+                         data=np.zeros((2000, 512, 512), dtype="uint8"))
+    yield str(fname)
+
+
+def test_sine_dewarp_end_to_end(input_h5, tmpdir):
+    output_h5 = str(tmpdir / "output.h5")
+    output_json = str(tmpdir / "output.json")
+    args = {
+            "input_h5": input_h5,
+            "output_h5": output_h5,
+            "equipment_name": "CAM2P.3",
+            "aL": "160.0",
+            "aR": "160.0",
+            "bL": "85.0",
+            "bR": "90.0",
+            "output_json": output_json}
+    smod = SineDewarp(input_data=args, args=[])
+    smod.run()
+
+    assert Path(output_h5).is_file()
+    assert Path(output_json).is_file()
+
+    with h5py.File(output_h5, "r") as f:
+        assert "data" in list(f.keys())
+        assert f['data'].shape[0:2] == (2000, 512)
+        assert f['data'].shape[2] < 512

ec3e8741f770b4c8b5a7629a61b7b4b3a2ce7116
danielsf <scott.daniel@alleninstitute.org>
2021-04-20 16:15:08 -0700

    add smoke tests for qc plot generation

--- tests/modules/decrosstalk/test_qc_plotting.py
+++ tests/modules/decrosstalk/test_qc_plotting.py
@@ -1,6 +1,12 @@
+import pathlib
+import json
+
 from ophys_etl.modules.decrosstalk.ophys_plane import OphysROI
+from ophys_etl.modules.decrosstalk.ophys_plane import DecrosstalkingOphysPlane
 from ophys_etl.modules.decrosstalk.qc_plotting import get_roi_pixels
 from ophys_etl.modules.decrosstalk.qc_plotting import find_overlapping_roi_pairs  # noqa: E501
+from ophys_etl.modules.decrosstalk.qc_plotting import generate_roi_figure
+from ophys_etl.modules.decrosstalk.qc_plotting import generate_pairwise_figures
 
 
 def test_get_roi_pixels():
@@ -129,3 +135,113 @@ def test_find_overlapping_roi_pairs():
     assert (0, 2, 1/5, 1/5) in overlap_list
     assert (4, 3, 1/11, 1/4) in overlap_list
     assert (4, 2, 1/11, 1/5) in overlap_list
+
+
+def test_summary_plot_generation(tmpdir):
+    """
+    Run a smoke test on qc_plotting.generate_roi_figure
+    """
+    this_dir = pathlib.Path(__file__).parent.resolve()
+    data_dir = this_dir / 'data/qc_plotting'
+
+    input_json_name = data_dir / 'DECROSSTALK_example_input.json'
+    with open(input_json_name, 'rb') as in_file:
+        src_data = json.load(in_file)
+
+    plane_list = []
+    for pair in src_data['coupled_planes']:
+        for plane in pair['planes']:
+
+            # redirect maximum projection path
+            orig = pathlib.Path(plane['maximum_projection_image_file']).name
+            new = data_dir / orig
+            plane['maximum_projection_image_file'] = new
+            p = DecrosstalkingOphysPlane.from_schema_dict(plane)
+
+            # add QC data file path to plane
+            qc_name = data_dir / f'{p.experiment_id}_qc_data.h5'
+
+            p.qc_file_path = qc_name
+            plane_list.append(p)
+
+    ophys_planes = []
+    for ii in range(0, len(plane_list),2):
+        ophys_planes.append((plane_list[ii], plane_list[ii+1]))
+
+    with open(data_dir / 'DECROSSTALK_example_output.json', 'rb') as in_file:
+        output_data = json.load(in_file)
+
+    out_path = tmpdir / 'summary.png'
+    assert not out_path.exists()
+
+    generate_roi_figure(1071648245,
+                        ophys_planes,
+                        output_data,
+                        tmpdir / 'summary.png')
+
+    assert out_path.isfile()
+
+
+def test_pairwise_plot_generation(tmpdir):
+    """
+    Run a smoke test on qc_plotting.generate_pairwise_figures
+    """
+    this_dir = pathlib.Path(__file__).parent.resolve()
+    data_dir = this_dir / 'data/qc_plotting'
+
+    input_json_name = data_dir / 'DECROSSTALK_example_input.json'
+    with open(input_json_name, 'rb') as in_file:
+        src_data = json.load(in_file)
+
+    plane_list = []
+    for pair in src_data['coupled_planes']:
+        for plane in pair['planes']:
+
+            # redirect maximum projection path
+            orig = pathlib.Path(plane['maximum_projection_image_file']).name
+            new = data_dir / orig
+            plane['maximum_projection_image_file'] = new
+            p = DecrosstalkingOphysPlane.from_schema_dict(plane)
+
+            # add QC data file path to plane
+            qc_name = data_dir / f'{p.experiment_id}_qc_data.h5'
+
+            p.qc_file_path = qc_name
+            plane_list.append(p)
+
+    ophys_planes = []
+    for ii in range(0, len(plane_list),2):
+        ophys_planes.append((plane_list[ii], plane_list[ii+1]))
+
+    with open(data_dir / 'DECROSSTALK_example_output.json', 'rb') as in_file:
+        output_data = json.load(in_file)
+
+
+    generate_pairwise_figures(ophys_planes,
+                              tmpdir)
+
+    expected_files = []
+    dirname = tmpdir / '1071738390_1071738393_roi_pairs'
+    for fname in ('1080616650_1080616555_comparison.png',
+                  '1080616658_1080616553_comparison.png',
+                  '1080616659_1080616554_comparison.png'):
+        expected_files.append(dirname / fname)
+
+    dirname = tmpdir / '1071738394_1071738396_roi_pairs'
+    for fname in ('1080618091_1080616600_comparison.png',
+                  '1080618102_1080616618_comparison.png'):
+        expected_files.append(dirname / fname)
+
+    dirname = tmpdir / '1071738397_1071738399_roi_pairs'
+    for fname in ('1080618093_1080623135_comparison.png',
+                  '1080618114_1080623164_comparison.png'):
+        expected_files.append(dirname / fname)
+
+    dirname = tmpdir / '1071738400_1071738402_roi_pairs'
+    for fname in ('1080616774_1080622865_comparison.png',
+                  '1080616776_1080622881_comparison.png'):
+        expected_files.append(dirname / fname)
+
+    for fname in expected_files:
+        if not fname.isfile():
+            raise RuntimeError(f"could not find {fname.resolve()}")

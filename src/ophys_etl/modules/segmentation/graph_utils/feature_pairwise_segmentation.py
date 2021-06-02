e6e8c92fd21682b75b0dce040e1b3dc580caefe3
danielsf <scott.daniel@alleninstitute.org>
2021-06-01 18:21:04 -0700

    paranoid

--- src/ophys_etl/modules/segmentation/graph_utils/feature_pairwise_segmentation.py
+++ src/ophys_etl/modules/segmentation/graph_utils/feature_pairwise_segmentation.py
@@ -190,8 +190,8 @@ class FeaturePairwiseSegmenter(FeatureVectorSegmenter):
         # pre-compute correlations
         img_shape = img_data.shape
         sqrt_proc = 2*np.ceil(np.sqrt(self.n_processors)).astype(int)
-        drow = img_shape[0]//sqrt_proc
-        dcol = img_shape[1]//sqrt_proc
+        drow = max(1, img_shape[0]//sqrt_proc)
+        dcol = max(1, img_shape[1]//sqrt_proc)
 
         t0 = time.time()
         p_list = []

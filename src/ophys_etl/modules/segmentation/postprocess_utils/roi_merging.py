b6ea18eb5b541bf9a732287c2fd4b6f57621c2e8
danielsf <scott.daniel@alleninstitute.org>
2021-06-08 12:00:17 -0700

    add method to check if ROIs abut each other

--- src/ophys_etl/modules/segmentation/postprocess_utils/roi_merging.py
+++ src/ophys_etl/modules/segmentation/postprocess_utils/roi_merging.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.distance import cdist
 from ophys_etl.types import ExtractROI
 from ophys_etl.modules.decrosstalk.ophys_plane import OphysROI
 from ophys_etl.modules.decrosstalk.ophys_plane import get_roi_pixels
@@ -65,3 +66,55 @@ def merge_rois(roi0: OphysROI,
                        valid_roi=True)
 
     return new_roi
+
+
+def _get_bdry_array(roi: OphysROI):
+    """
+    get Nx2 array of pixels (in global coordinates)
+    that are on boundary of ROI
+    """
+    bdry_mask = roi.boundary_mask
+    n_bdry = bdry_mask.sum()
+    bdry_array = -1*np.ones((n_bdry, 2), dtype=int)
+    i_pix = 0
+    for ir in range(roi.height):
+        row = ir+roi.y0
+        for ic in range(roi.width):
+            col =ic+roi.x0
+            if not bdry_mask[ir, ic]:
+                continue
+
+            bdry_array[i_pix, 0] = row
+            bdry_array[i_pix, 1] = col
+            i_pix += 1
+
+    if bdry_array.min() < 0:
+        raise RuntimeError("did not assign all boundary pixels")
+
+    return bdry_array
+
+
+def do_rois_abut(roi0: OphysROI,
+                 roi1: OphysROI,
+                 dpix: float = 1) -> bool:
+    """
+    Returns True if ROIs are within dpix of each other at any point along
+    their boundaries
+
+    Note: dpix is such that if two boundaries are next to each other,
+    that is dpix=1; dpix=2 is a 1 blank pixel between ROIs
+    """
+
+    # do the ROIs overlap
+    pixel_dict = get_roi_pixels([roi0, roi1])
+    intersection = pixel_dict[roi0.roi_id].intersection(pixel_dict[roi1.roi_id])
+    if len(intersection) > 0:
+        return True
+
+    bdry_0 = _get_bdry_array(roi0)
+    bdry_1 = _get_bdry_array(roi1)
+
+    distances = cdist(bdry_0, bdry_1, metric='euclidean')
+    if distances.min() <= dpix:
+        return True
+    return False

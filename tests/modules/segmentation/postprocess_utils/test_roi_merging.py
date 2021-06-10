66d4d2e67df0ee4bf077dc3f97dc8feb9bccc389
danielsf <scott.daniel@alleninstitute.org>
2021-06-09 22:08:52 -0700

    unit test for make_cdf

--- tests/modules/segmentation/postprocess_utils/test_roi_merging.py
+++ tests/modules/segmentation/postprocess_utils/test_roi_merging.py
@@ -5,7 +5,8 @@ from ophys_etl.modules.decrosstalk.ophys_plane import OphysROI
 from ophys_etl.modules.segmentation.postprocess_utils.roi_merging import (
     merge_rois,
     do_rois_abut,
-    correlate_sub_videos)
+    correlate_sub_videos,
+    make_cdf)
 
 
 def test_merge_rois():
@@ -165,3 +166,17 @@ def test_correlate_sub_videos():
             val = np.mean((trace0-mu0)*(trace1-mu1))
             val = val/np.sqrt(var1*var0)
             assert np.abs((val-corr[ipix0,ipix1])/val)<1.0e-10
+
+
+def test_cdf():
+    rng = np.random.RandomState(221144)
+    data_set_list = [rng.random_sample(200)*5.0,
+                     rng.normal(5.0, 2.0, size=300)]
+    for data_set in data_set_list:
+        (bins,
+         cdf) = make_cdf(data_set)
+        test = rng.random_sample(50)*5.2
+        interped_cdf = np.interp(test, bins, cdf)
+        for xx, yy in zip(test, interped_cdf):
+            ct = (data_set<xx).sum()
+            assert np.abs(yy-ct/len(data_set)) < 1.0e-2

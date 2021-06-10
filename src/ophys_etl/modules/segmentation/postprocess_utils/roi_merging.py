d8f751beffe4e7a6254253b5f0003caaf89af5ef
danielsf <scott.daniel@alleninstitute.org>
2021-06-10 10:46:04 -0700

    actually, just have method to calculate chisq per dof

--- src/ophys_etl/modules/segmentation/postprocess_utils/roi_merging.py
+++ src/ophys_etl/modules/segmentation/postprocess_utils/roi_merging.py
@@ -336,17 +336,15 @@ def create_self_correlation_lookup(roi_list: List[OphysROI],
     return final_output
 
 
-def evaluate_merger_chisq(large_self_corr: np.ndarray,
-                          cross_corr: np.ndarray,
-                          p_value: float):
+def calculate_merger_chisq(large_self_corr: np.ndarray,
+                          cross_corr: np.ndarray):
     """
     Take the self correlation of a large ROI and
     the cross-correlation between a smaller ROI and
     the larger ROI. Find the probability that the
     cross correlation values fall within the
     CDF of the larger cross correlations. Sum those
-    to make a chisquared. If chisquared <= ln(p_value),
-    return True. Else, return False.
+    to make a chisquared. Return chisq per dof
     """
     (cdf_bins,
      cdf_vals) = make_cdf(large_self_corr)
@@ -359,8 +357,4 @@ def evaluate_merger_chisq(large_self_corr: np.ndarray,
     chisq = -2.0*np.log(prob).sum()
     chisq_per_dof = chisq/len(cross_corr)
 
-    target_chisq = -2.0*np.log(p_value)
-
-    if chisq_per_dof <= target_chisq:
-        return True
-    return False
+    return chisq_per_dof

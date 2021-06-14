1e3af89017898fa1637f3753b1d96970ce52f77c
danielsf <scott.daniel@alleninstitute.org>
2021-06-14 12:34:45 -0700

    rough geometric merging implementation

--- src/ophys_etl/modules/segmentation/modules/roi_merging.py
+++ src/ophys_etl/modules/segmentation/modules/roi_merging.py
@@ -10,6 +10,8 @@ from ophys_etl.modules.segmentation.modules.schemas import \
 
 import ophys_etl.modules.segmentation.postprocess_utils.roi_merging as merging
 from ophys_etl.modules.segmentation.graph_utils.plotting import graph_to_img
+from ophys_etl.modules.segmentation.postprocess_utils.roi_geom_merging import (
+    do_geometric_merger)
 import networkx
 
 import logging
@@ -65,39 +67,16 @@ class RoiMergerEngine(argschema.ArgSchemaParser):
             whole_video = in_file['data'][()]
 
         if self.args['graph_input'] is None:
+            raise RuntimeError("must specify graph_input")
             graph_img = None
         else:
             graph_img = graph_to_img(networkx.read_gpickle(self.args['graph_input']),
                                      attribute_name='filtered_hnc_Gaussian')
 
-        shuffler = np.random.RandomState(551234)
-        keep_going = True
-        i_pass = -1
-        unchanged_roi = set()
-        while keep_going:
-            i_pass += 1
-            n_roi_0 = len(roi_list)
-
-            (keep_going,
-             roi_list,
-             unchanged_roi) = merging.attempt_merger_pixel_correlation(
-                                        whole_video,
-                                        roi_list,
-                                        self.args['filter_fraction'],
-                                        shuffler,
-                                        self.args['n_parallel_workers'],
-                                        unchanged_roi=unchanged_roi,
-                                        img_data=graph_img,
-                                        i_pass=i_pass,
-                                        diagnostic_dir=diagnostic_dir)
-            n_roi_1 = len(roi_list)
-            duration = time.time()-t0
-            self.logger.info(f'Merged {n_roi_0} ROIs to {n_roi_1} '
-                             f'after {duration:.2f} seconds')
-            if diagnostic_dir is not None:
-                intermediate_out = diagnostic_dir / f'rois_{i_pass}.json'
-                write_out_rois(roi_list, intermediate_out)
-
+        roi_list = do_geometric_merger(roi_list,
+                                       graph_img,
+                                       self.args['n_parallel_workers'],
+                                       diagnostic_dir=diagnostic_dir)
 
         write_out_rois(roi_list, self.args['roi_output'])
 

3f44ee0c15f4b665d1028a4496671b82147ab508
danielsf <scott.daniel@alleninstitute.org>
2021-06-17 17:37:32 -0700

    remove unused methods

--- src/ophys_etl/modules/segmentation/postprocess_utils/roi_bayes.py
+++ src/ophys_etl/modules/segmentation/postprocess_utils/roi_bayes.py
@@ -2,37 +2,6 @@ import numpy as np
 from typing import Tuple
 from ophys_etl.modules.segmentation.postprocess_utils.roi_types import (
     SegmentationROI)
-from sklearn.decomposition import PCA as sklearn_PCA
-
-
-def chisq_from_video(sub_video: np.ndarray,
-                     fit_video: np.ndarray,
-                     n_components=3):
-    """
-    sub_video is (nt, npix)
-    """
-    if sub_video.shape[1] < (n_components+1):
-        return 0.0
-    npix = sub_video.shape[1]
-    ntime = sub_video.shape[0]
-    sub_video = sub_video.T
-
-    pca = sklearn_PCA(n_components=n_components, copy=True)
-
-    pca.fit(fit_video)
-    transformed_video = pca.fit_transform(sub_video)
-
-    assert transformed_video.shape==(sub_video.shape[0], n_components)
-
-    mu = np.mean(transformed_video, axis=0)
-    assert mu.shape == (n_components,)
-    distances = np.sqrt(((transformed_video-mu)**2).sum(axis=1))
-    assert distances.shape == (npix,)
-    std = np.std(distances, ddof=1)
-    chisq = ((transformed_video-mu)/std)**2
-    chisq = chisq.sum()
-
-    return chisq+2.0*n_components*npix*np.log(std)+n_components*npix*np.log(2.0*np.pi)
 
 
 def sub_video_from_roi(roi: SegmentationROI,
@@ -144,42 +113,3 @@ def validate_merger_corr(uphill_roi: SegmentationROI,
     z_score = (downhill_to_uphill-uphill_mu)/uphill_std
     metric = np.median(z_score)
     return metric>(-1.0*acceptance)
-
-
-
-def validate_merger_bic(uphill_roi: SegmentationROI,
-                        downhill_roi: SegmentationROI,
-                        video_data: np.ndarray,
-                        n_components: int = 3):
-    (uphill_video,
-     uphill_centroid) = sub_video_from_roi(uphill_roi, video_data)
-
-    (downhill_video,
-     downhill_centroid) = sub_video_from_roi(downhill_roi, video_data)
-
-    npix_uphill = uphill_video.shape[1]
-    npix_downhill = downhill_video.shape[1]
-    npix = npix_uphill+npix_downhill
-    ntime = video_data.shape[0]
-
-    merger_video = np.zeros((ntime,
-                             npix_uphill+npix_downhill), dtype=float)
-    merger_video[:, :npix_uphill] = uphill_video
-    merger_video[:, npix_uphill:] = downhill_video
-
-    chisq_uphill = chisq_from_video(uphill_video,
-                                    uphill_video,
-                                    n_components=n_components)
-
-    chisq_downhill = chisq_from_video(downhill_video,
-                                      downhill_video,
-                                      n_components=n_components)
-
-    chisq_merger = chisq_from_video(merger_video,
-                                    merger_video,
-                                    n_components=n_components)
-
-    bic_baseline = 4*n_components*np.log(npix) + chisq_uphill + chisq_downhill
-    bic_merger = 2*n_components*np.log(npix) + chisq_merger
-    d_bic = bic_merger-bic_baseline
-    return (d_bic<0.0)

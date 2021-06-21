cb0d5fb5009809c962a3ca6d3b176786807686c3
Dan <danielk@alleninstitute.org>
2021-06-21 12:22:30 -0700

    remove I/O from seed qc plot

--- tests/modules/segmentation/qc/test_qc_seed.py
+++ tests/modules/segmentation/qc/test_qc_seed.py
@@ -8,7 +8,7 @@ from ophys_etl.modules.segmentation.seed import seeder
 
 
 @pytest.fixture
-def seed_h5_file(tmp_path):
+def seed_h5_group(tmp_path):
     image = np.array([[0.0, 0.0, 0.4, 0.0],
                       [0.5, 0.0, 0.3, 0.0],
                       [0.0, 0.5, 0.0, 0.0],
@@ -24,44 +24,23 @@ def seed_h5_file(tmp_path):
     with h5py.File(h5path, "w") as f:
         sb.log_to_h5_group(f)
 
-    yield h5path
+    with h5py.File(h5path, "r") as f:
+        yield f["seeding"]
 
 
 @pytest.mark.parametrize(
         "image_background",
         [None, np.zeros((512, 512)), "seed_image"])
-def test_add_seeds_to_axes_default(seed_h5_file,  image_background):
+def test_add_seeds_to_axes_default(seed_h5_group,  image_background):
     fig = Figure()
     axes = fig.add_subplot(111)
     qcseed.add_seeds_to_axes(figure=fig,
                              axes=axes,
-                             seed_h5_path=seed_h5_file,
+                             seed_h5_group=seed_h5_group,
                              image_background=image_background)
 
 
-def test_add_seeds_to_axes_from_group(seed_h5_file):
-    fig = Figure()
-    axes = fig.add_subplot(111)
-    with h5py.File(seed_h5_file, "r") as f:
-        group = f["seeding"]
-        qcseed.add_seeds_to_axes(figure=fig,
-                                 axes=axes,
-                                 seed_h5_path=None,
-                                 seed_h5_group=group)
-
-
-def test_add_seeds_no_group(seed_h5_file):
-    fig = Figure()
-    axes = fig.add_subplot(111)
-    with pytest.raises(ValueError,
-                       match=r"for (seed_h5_path, seed_h5_group)*"):
-        qcseed.add_seeds_to_axes(figure=fig,
-                                 axes=axes,
-                                 seed_h5_path=seed_h5_file,
-                                 seed_h5_group=None)
-
-
-def test_add_seeds_bad_image(seed_h5_file, tmp_path):
+def test_add_seeds_bad_image(seed_h5_group, tmp_path):
     junk = tmp_path / "an_image.png"
     fig = Figure()
     axes = fig.add_subplot(111)
@@ -69,5 +48,5 @@ def test_add_seeds_bad_image(seed_h5_file, tmp_path):
                        match=r"image background should be *"):
         qcseed.add_seeds_to_axes(figure=fig,
                                  axes=axes,
-                                 seed_h5_path=seed_h5_file,
+                                 seed_h5_group=seed_h5_group,
                                  image_background=junk)

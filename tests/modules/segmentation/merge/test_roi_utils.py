c16136c085353d511587dc159499b4a5da012805
danielsf <scott.daniel@alleninstitute.org>
2021-06-29 09:51:24 -0700

    remove redundant pytest fixture

--- tests/modules/segmentation/merge/test_roi_utils.py
+++ tests/modules/segmentation/merge/test_roi_utils.py
@@ -10,26 +10,6 @@ from ophys_etl.modules.segmentation.merge.roi_utils import (
     ophys_roi_to_extract_roi)
 
 
-@pytest.fixture
-def example_roi_list():
-    rng = np.random.RandomState(6412439)
-    roi_list = []
-    for ii in range(30):
-        x0 = rng.randint(0, 25)
-        y0 = rng.randint(0, 25)
-        height = rng.randint(3, 7)
-        width = rng.randint(3, 7)
-        mask = rng.randint(0, 2, (height, width)).astype(bool)
-        roi = OphysROI(x0=x0, y0=y0,
-                       height=height, width=width,
-                       mask_matrix=mask,
-                       roi_id=ii,
-                       valid_roi=True)
-        roi_list.append(roi)
-
-    return roi_list
-
-
 def test_extract_roi_to_ophys_roi():
     rng = np.random.RandomState(345)
     mask = rng.randint(0, 2, (9, 7)).astype(bool)

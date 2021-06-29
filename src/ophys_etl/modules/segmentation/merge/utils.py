de294f125e4b55c602b7e243ec1202415b9604ef
danielsf <scott.daniel@alleninstitute.org>
2021-06-29 09:29:30 -0700

    typing and documentation in utils.py

--- src/ophys_etl/modules/segmentation/merge/utils.py
+++ src/ophys_etl/modules/segmentation/merge/utils.py
@@ -1,4 +1,21 @@
-def _winnow_process_list(process_list):
+from multiprocessing import Process
+from typing import List
+
+
+def _winnow_process_list(
+        process_list: List[Process]) -> List[Process]:
+    """
+    Loop over a list of processes, popping out any that have
+    been completed. Return the winnowed list of processes.
+
+    Parameters
+    ----------
+    process_list: List[multiprocessing.Process]
+
+    Returns
+    -------
+    process_list: List[multiprocessing.Process]
+    """
     to_pop = []
     for ii in range(len(process_list)-1, -1, -1):
         if process_list[ii].exitcode is not None:

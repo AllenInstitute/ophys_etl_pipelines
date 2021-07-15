7f1ab1caa13c171344a64fee9289ee60ff74d428
danielsf <scott.daniel@alleninstitute.org>
2021-07-14 17:15:41 -0700

    add intersection_over_union infrastructure to OphysROI

--- tests/modules/decrosstalk/test_ophys_plane.py
+++ tests/modules/decrosstalk/test_ophys_plane.py
@@ -1,7 +1,9 @@
 import os
 import json
+import numpy as np
 from ophys_etl.modules.decrosstalk.ophys_plane import OphysROI
 from ophys_etl.modules.decrosstalk.ophys_plane import DecrosstalkingOphysPlane
+from ophys_etl.modules.decrosstalk.ophys_plane import intersection_over_union
 
 from .utils import get_data_dir
 
@@ -53,3 +55,58 @@ def test_setting_qc_path():
     assert plane.qc_file_path is None
     plane.qc_file_path = 'path/to/a/file'
     assert plane.qc_file_path == 'path/to/a/file'
+
+
+def test_roi_pixel_set():
+    width = 7
+    height = 5
+    mask = np.zeros((height, width), dtype=bool)
+    mask[2, 4] = True
+    mask[3, 6] = True
+    roi = OphysROI(roi_id=1,
+                   x0=100,
+                   y0=200,
+                   width=width,
+                   height=height,
+                   valid_roi=True,
+                   mask_matrix=mask)
+    assert roi.pixel_set == set([(202, 104), (203, 106)])
+
+
+def test_intersection_over_union():
+
+    width = 7
+    height = 5
+    mask = np.ones((height, width), dtype=bool)
+    mask[:, 4:] = False
+    roi0 = OphysROI(roi_id=0,
+                    x0=100,
+                    y0=200,
+                    width=width,
+                    height=height,
+                    valid_roi=True,
+                    mask_matrix=mask)
+
+    width = 4
+    height = 9
+    mask = np.ones((height, width), dtype=bool)
+    mask[:, 0] = False
+    mask[2:, :] = False
+    roi1 = OphysROI(roi_id=0,
+                    x0=101,
+                    y0=201,
+                    width=width,
+                    height=height,
+                    valid_roi=True,
+                    mask_matrix=mask)
+
+    # expected_intersection = 4
+    # expected_union = 22
+
+    expected = 4.0/22.0
+
+    actual = intersection_over_union(roi0, roi1)
+    actual1 = intersection_over_union(roi1, roi0)
+    eps = 1.0e-20
+    assert np.abs(actual-actual1) < eps
+    assert np.abs(actual-expected) < eps

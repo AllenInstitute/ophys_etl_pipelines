1a614e55f9333141de034f79c6e1b49fb5453164
danielsf <scott.daniel@alleninstitute.org>
2021-07-28 08:42:07 -0700

    factor out method to add one ROI boundary to an image

--- src/ophys_etl/modules/segmentation/qc_utils/roi_utils.py
+++ src/ophys_etl/modules/segmentation/qc_utils/roi_utils.py
@@ -63,6 +63,47 @@ def add_roi_mask_to_img(
     return img
 
 
+def add_roi_boundary_to_img(
+        img: np.ndarray,
+        roi: OphysROI,
+        color: Tuple[int],
+        alpha: float) -> np.ndarray:
+    """
+    Add colored ROI boundary to an image
+
+    Parameters
+    ----------
+    img: np.ndarray
+        RGB representation of image
+
+    roi: OphysROI
+
+    color: Tuple[int]
+        RGB color of ROI
+
+    alpha: float
+
+    Returns
+    -------
+    img: np.ndarray
+
+    Note
+    ----
+    While this function does return an image, it also operates
+    on img in place
+    """
+    bdry = roi.boundary_mask
+    valid = np.argwhere(bdry)
+    rows = np.array([r+roi.y0 for r in valid[:, 0]])
+    cols = np.array([c+roi.x0 for c in valid[:, 1]])
+    for ic in range(3):
+        old_vals = img[rows, cols, ic]
+        new_vals = np.round(alpha*color[ic]+(1.0-alpha)*old_vals).astype(int)
+        img[rows, cols, ic] = new_vals
+    img = np.where(img >= 255, 255, img)
+    return img
+
+
 def add_list_of_roi_boundaries_to_img(
         img: np.ndarray,
         roi_list: Union[List[OphysROI], List[Dict]],
@@ -101,20 +142,11 @@ def add_list_of_roi_boundaries_to_img(
                     for roi in roi_list]
 
     for roi in roi_list:
-        bdry = roi.boundary_mask
-        for icol in range(roi.width):
-            for irow in range(roi.height):
-                if not bdry[irow, icol]:
-                    continue
-                yy = roi.y0 + irow
-                xx = roi.x0 + icol
-                for ic in range(3):
-                    old_val = np.round(img[yy, xx, ic]*(1.0-alpha)).astype(int)
-                    new_img[yy, xx, ic] = old_val
-                    new_val = np.round(alpha*color[ic]).astype(int)
-                    new_img[yy, xx, ic] += new_val
-
-    new_img = np.where(new_img <= 255, new_img, 255)
+        new_img = add_roi_boundary_to_img(
+                      new_img,
+                      roi,
+                      color,
+                      alpha)
     return new_img
 
 
--- tests/modules/segmentation/qc_utils/test_qc_roi_utils.py
+++ tests/modules/segmentation/qc_utils/test_qc_roi_utils.py
@@ -67,6 +67,48 @@ def test_roi_average_metric(roi_list, metric_image, expected):
 
 
 @pytest.mark.parametrize('alpha', [0.2, 0.3, 0.4])
+def test_add_roi_boundary_to_img(alpha):
+    img = 100*np.ones((64, 64, 3), dtype=int)
+
+    height = 7
+    width = 12
+
+    mask = np.zeros((height, width), dtype=bool)
+    mask[1, 5:7] = True
+    mask[2, 4:8] = True
+    mask[3, 3:9] = True
+    mask[4, 2:10] = True
+    mask[5, 3:9] = True
+
+    bdry_pixels = set([(1, 5), (1, 6), (2, 4), (2, 7),
+                       (3, 3), (3, 8), (4, 2), (4, 9),
+                       (5, 3), (5, 4), (5, 5), (5, 6),
+                       (5, 7), (5, 8)])
+
+    roi = OphysROI(x0=20, width=width,
+                   y0=15, height=height,
+                   valid_roi=True, roi_id=0,
+                   mask_matrix=mask)
+
+    color = (22, 33, 44)
+    img = roi_utils.add_roi_boundary_to_img(
+                      img,
+                      roi,
+                      color,
+                      alpha)
+
+    for row in range(height):
+        for col in range(width):
+            for ic in range(3):
+                if (row, col) not in bdry_pixels:
+                    assert img[15+row, 20+col, ic] == 100
+                else:
+                    expected = np.round(alpha*color[ic]
+                                        + (1.0-alpha)*100).astype(int)
+                    assert img[15+row, 20+col, ic] == expected
+
+
+@pytest.mark.parametrize('alpha', [0.2, 0.3, 0.4])
 def test_add_roi_mask_to_img(alpha):
     rng = np.random.default_rng(634212)
     img = 100*np.ones((64, 64, 3), dtype=int)

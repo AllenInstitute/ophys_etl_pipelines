17306701d1337672557c6ed7bd2e4f0189a16c09
danielsf <scott.daniel@alleninstitute.org>
2021-07-29 11:13:03 -0700

    add median filter utility; rename 'average' -> 'mean'

--- src/ophys_etl/modules/segmentation/filter/filter_utils.py
+++ src/ophys_etl/modules/segmentation/filter/filter_utils.py
@@ -3,11 +3,11 @@ import numpy as np
 from ophys_etl.modules.decrosstalk.ophys_plane import OphysROI
 
 
-def average_metric_from_roi(
+def mean_metric_from_roi(
         roi: OphysROI,
         img: np.ndarray) -> float:
     """
-    Calculate the average metric value in an ROI based on
+    Calculate the mean metric value in an ROI based on
     a provided metric image
 
     Parameters
@@ -21,10 +21,37 @@ def average_metric_from_roi(
     Returns
     -------
     avg_metric_value: float
-        The average value of pixels in img that are a part
+        The mean value of pixels in img that are a part
         of the ROI
     """
 
     rows = roi.global_pixel_array[:, 0]
     cols = roi.global_pixel_array[:, 1]
     return np.mean(img[rows, cols])
+
+
+def median_metric_from_roi(
+        roi: OphysROI,
+        img: np.ndarray) -> float:
+    """
+    Calculate the median metric value in an ROI based on
+    a provided metric image
+
+    Parameters
+    ----------
+    roi: OphysROI
+
+    img: np.ndarray
+        The metric image whose average value in the ROI is
+        being returned. Shape is (nrows, ncolumns)
+
+    Returns
+    -------
+    avg_metric_value: float
+        The median value of pixels in img that are a part
+        of the ROI
+    """
+
+    rows = roi.global_pixel_array[:, 0]
+    cols = roi.global_pixel_array[:, 1]
+    return np.median(img[rows, cols])
--- tests/modules/segmentation/filter/test_filter_utils.py
+++ tests/modules/segmentation/filter/test_filter_utils.py
@@ -2,7 +2,8 @@ import pytest
 import numpy as np
 from ophys_etl.modules.decrosstalk.ophys_plane import OphysROI
 from ophys_etl.modules.segmentation.filter.filter_utils import (
-    average_metric_from_roi)
+    mean_metric_from_roi,
+    median_metric_from_roi)
 
 
 @pytest.fixture(scope='session')
@@ -17,10 +18,26 @@ def image_fixture():
          (20, 5, [[True, True, False, False],
                   [False, True, False, False],
                   [True, True, True, True]], 222.28571)])
-def test_avg_from_roi(image_fixture, x0, y0, mask, expected):
+def test_mean_from_roi(image_fixture, x0, y0, mask, expected):
     roi = OphysROI(x0=x0, width=len(mask[0]),
                    y0=y0, height=len(mask),
                    valid_roi=True, roi_id=0,
                    mask_matrix=mask)
-    actual = average_metric_from_roi(roi, image_fixture)
+    actual = mean_metric_from_roi(roi, image_fixture)
+    assert np.allclose(actual, expected, rtol=0.0, atol=0.0001)
+
+
+@pytest.mark.parametrize(
+        "x0, y0, mask, expected",
+        [(12, 15, [[True, False, True], [False, True, False]], 494.0),
+         (20, 5, [[True, True, False, False],
+                  [False, True, False, False],
+                  [True, True, True, True]], 244.0),
+         (11, 9, [[True, False, True], [True, True, False]], 316.0)])
+def test_median_from_roi(image_fixture, x0, y0, mask, expected):
+    roi = OphysROI(x0=x0, width=len(mask[0]),
+                   y0=y0, height=len(mask),
+                   valid_roi=True, roi_id=0,
+                   mask_matrix=mask)
+    actual = median_metric_from_roi(roi, image_fixture)
     assert np.allclose(actual, expected, rtol=0.0, atol=0.0001)

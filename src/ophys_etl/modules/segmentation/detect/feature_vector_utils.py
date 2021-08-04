1d52a7f80e216f68e2cf59b126dc2d3d50a3e0c3
danielsf <scott.daniel@alleninstitute.org>
2021-08-04 08:16:51 -0700

    only use seed point to choose timesteps

--- src/ophys_etl/modules/segmentation/detect/feature_vector_utils.py
+++ src/ophys_etl/modules/segmentation/detect/feature_vector_utils.py
@@ -50,41 +50,7 @@ def choose_timesteps(
     thresh = np.quantile(trace, discard)
     global_mask = []
     mask = np.where(trace >= thresh)[0]
-    global_mask.append(mask)
-
-    # choose n_seeds other points to populate global_mask
-    possible_seeds = []
-    for rr, cc in product(range(sub_video.shape[1]),
-                          range(sub_video.shape[2])):
-        pt = (rr, cc)
-        if pt == seed_pt:
-            continue
-        if pixel_ignore is None or not pixel_ignore[rr, cc]:
-            possible_seeds.append(pt)
-
-    possible_seed_indexes = np.arange(len(possible_seeds))
-    n_seeds = 10
-    if rng is None:
-        rng = np.random.RandomState(87123)
-    chosen = set()
-    chosen.add(seed_pt)
-
-    if len(possible_seeds) > n_seeds:
-        chosen_seeds = rng.choice(possible_seed_indexes,
-                                  size=n_seeds, replace=False)
-        for ii in chosen_seeds:
-            chosen.add(possible_seeds[ii])
-    else:
-        for ii in possible_seed_indexes:
-            chosen.add(possible_seeds[ii])
-
-    for chosen_pixel in chosen:
-        trace = sub_video[:, chosen_pixel[0], chosen_pixel[1]]
-        thresh = np.quantile(trace, discard)
-        mask = np.where(trace >= thresh)[0]
-        global_mask.append(mask)
-    global_mask = np.unique(np.concatenate(global_mask))
-    return global_mask
+    return mask
 
 
 def select_window_size(

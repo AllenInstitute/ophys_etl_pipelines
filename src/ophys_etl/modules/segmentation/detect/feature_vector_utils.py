0895ce5fbbb737d3d5d700bc0790aceba21baaee
danielsf <scott.daniel@alleninstitute.org>
2021-08-04 14:34:37 -0700

    add test for select_window_size

--- src/ophys_etl/modules/segmentation/detect/feature_vector_utils.py
+++ src/ophys_etl/modules/segmentation/detect/feature_vector_utils.py
@@ -89,37 +89,88 @@ def choose_timesteps(
 
 def select_window_size(
         seed_pt: Tuple[int, int],
-        img: np.ndarray,
+        image_data: np.ndarray,
         target_z_score: float=2.0,
         window_min: int=20,
         window_max: int=300,
-        pixel_ignore=None) -> int:
+        pixel_ignore: Optional[np.ndarray]=None) -> int:
+    """
+    For an image, find the window half side length
+    centered on a pixel such that the specified pixel
+    is at least N-sigma brighter than the distribution
+    of pixels the image.
 
+    Parameters
+    ----------
+    seed_pt: Tuple[int, int]
+        (row, col) of the specified point
+
+    image_data: np.ndarray
+
+    target_z_score: float
+        The target z-score of seed_pt within image
+        (default=2.0)
+
+    window_min: int
+        Minimum window size to return
+
+    window_max: int
+        Maximum window size to return. If the window size
+        exceeds this limit without matching or exceeding
+        target_z_score, return the last window size tested.
+
+    pixel_ignore: Optional[np.ndarray]
+        A mask marked as True for pixels that are to be
+        ignored when calculating the z-score of seed_pt
+
+    Returns
+    -------
+    window: int
+        The half side length (in pixels) of a window
+        centered on seed_pt such that seed_pt is at
+        least target_z_score standard deviations
+        brighter than all of the pixels in the window
+
+    Notes
+    -----
+    This method starts with a window size of window_min.
+    It grows the window by a factor for 3/2 at each step.
+    It returns the first value of window that either
+    exceeds window_max or results in a z-score exceeding
+    target_z_score.
+    """
     if pixel_ignore is not None:
+        if pixel_ignore.shape != image_data.shape:
+            msg = f"pixel_ignore.shape {pixel_ignore.shape}\n"
+            msg += f"image_data.shape {image_data.shape}\n"
+            msg += "These should be the same"
+            raise RuntimeError(msg)
         pixel_use = np.logical_not(pixel_ignore)
     else:
-        pixel_use = np.ones(img.shape, dtype=bool)
+        pixel_use = np.ones(image_data.shape, dtype=bool)
 
     window = None
-    seed_flux = img[seed_pt[0], seed_pt[1]]
+    seed_flux = image_data[seed_pt[0], seed_pt[1]]
     z_score = 0.0
     while z_score < target_z_score:
         if window is None:
             window = window_min
         else:
-            window = 3*window//2
+            window = max(3*window//2, window+1)
         r0 = max(0, seed_pt[0]-window)
-        r1 = min(img.shape[0], seed_pt[0]+window+1)
+        r1 = min(image_data.shape[0], seed_pt[0]+window+1)
         c0 = max(0, seed_pt[1]-window)
-        c1 = min(img.shape[1], seed_pt[1]+window+1)
+        c1 = min(image_data.shape[1], seed_pt[1]+window+1)
         local_mask = pixel_use[r0:r1, c0:c1]
-        local_img = img[r0:r1, c0:c1]
-        background = local_img[local_mask].flatten()
+        local_image = image_data[r0:r1, c0:c1]
+        background = local_image[local_mask].flatten()
         mu = np.mean(background)
         q25, q75 = np.quantile(background, (0.25, 0.75))
-        std = (q75-q25)/1.34896
+        std = max(1.0e-10, (q75-q25)/1.34896)
         z_score = (seed_flux-mu)/std
         if window >= window_max:
             break
-    print(f'seed {seed_pt} window {window} -- z {z_score:.2e} -- {seed_flux:.2e} {mu:.2e} {std:.2e}')
+        if r0==0 and r1==image_data.shape[0]:
+            if c0==0 and c1==image_data.shape[1]:
+                break
     return window
--- tests/modules/segmentation/detect/test_feature_vector_utils.py
+++ tests/modules/segmentation/detect/test_feature_vector_utils.py
@@ -1,21 +1,23 @@
 import pytest
 import numpy as np
+from itertools import product
 
 from ophys_etl.modules.segmentation.detect.feature_vector_utils import (
-    choose_timesteps)
+    choose_timesteps,
+    select_window_size)
 
 
 def test_choose_timesteps():
     rng = np.random.default_rng(16232213)
     movie_shape = (100, 32, 32)
 
-    img_data = rng.normal(0.0, 0.1, movie_shape[1:])
+    image_data = rng.normal(0.0, 0.1, movie_shape[1:])
     movie_data = rng.normal(0.0, 0.1, movie_shape)
 
-    img_data[10, 15] = 1.0
-    img_data[7, 12] = 0.9
-    img_data[13, 11] = 0.8
-    img_data[12, 17] = 0.75
+    image_data[10, 15] = 1.0
+    image_data[7, 12] = 0.9
+    image_data[13, 11] = 0.8
+    image_data[12, 17] = 0.75
 
     mx = movie_data.max()
 
@@ -38,14 +40,14 @@ def test_choose_timesteps():
                     movie_data,
                     seed_pt,
                     0.2,
-                    img_data,
+                    image_data,
                     pixel_ignore=np.zeros((4, 4), dtype=bool))
 
     timesteps = choose_timesteps(
                     movie_data,
                     seed_pt,
                     0.15,
-                    img_data)
+                    image_data)
 
     expected = np.concatenate([range(0, 15),
                                range(20, 35),
@@ -56,7 +58,7 @@ def test_choose_timesteps():
                     movie_data,
                     seed_pt,
                     0.2,
-                    img_data)
+                    image_data)
 
     expected = np.concatenate([range(0, 20),
                                range(20, 40),
@@ -71,7 +73,7 @@ def test_choose_timesteps():
                     movie_data,
                     seed_pt,
                     0.15,
-                    img_data,
+                    image_data,
                     pixel_ignore=mask)
 
     expected = np.concatenate([range(0, 15),
@@ -84,7 +86,7 @@ def test_choose_timesteps():
                     movie_data,
                     seed_pt,
                     0.2,
-                    img_data,
+                    image_data,
                     pixel_ignore=mask)
 
     expected = np.concatenate([range(0, 20),
@@ -92,3 +94,101 @@ def test_choose_timesteps():
                                range(65, 85)])
 
     np.testing.assert_array_equal(expected, timesteps)
+
+
+@pytest.fixture(scope='session')
+def image_fixture():
+
+    rng = np.random.default_rng(76143232)
+    image_data = rng.normal(0.0, 0.1, (32, 32))
+    return image_data
+
+
+@pytest.fixture(scope='session')
+def mask_fixture():
+    rng = np.random.default_rng(554433)
+    return rng.integers(0, 2, (32, 32)).astype(bool)
+
+
+@pytest.mark.parametrize(
+        'seed_pt',
+        [(10, 11), (0, 3), (3, 0), (31, 5), (6, 31)])
+def test_select_window_size(image_fixture, mask_fixture, seed_pt):
+
+    window_min = 5
+
+    # put a "cell" in the image
+    image_data = np.copy(image_fixture)
+
+    rows, cols = np.meshgrid(range(32), range(32), indexing='ij')
+    dsq = (rows-seed_pt[0])**2+(cols-seed_pt[1])**2
+    valid = dsq < 36.0
+
+    image_data[valid] += 1.0
+
+    with pytest.raises(RuntimeError, match='These should be the same'):
+        select_window_size(
+                    seed_pt,
+                    image_data,
+                    2.0,
+                    window_min=window_min,
+                    window_max=1000,
+                    pixel_ignore=np.zeros((2, 2), dtype=bool))
+
+    found_z = 0
+    for z_score, mask in product((1.0, 6.2, 8.4, 2.0e6),
+                                 (None, mask_fixture)):
+        window = select_window_size(
+                    seed_pt,
+                    image_data,
+                    z_score,
+                    window_min=window_min,
+                    window_max=1000,
+                    pixel_ignore=mask)
+
+        # calculate the z-score associated with this window
+        r0 = max(0, seed_pt[0]-window)
+        r1 = min(image_data.shape[0], seed_pt[0]+window+1)
+        c0 = max(0, seed_pt[1]-window)
+        c1 = min(image_data.shape[1], seed_pt[1]+window+1)
+        local_image = image_data[r0:r1, c0:c1].flatten()
+        if mask is not None:
+            local_image = local_image[
+                            np.logical_not(mask[r0:r1, c0:c1]).flatten()]
+
+        mu = np.mean(local_image)
+        q25, q75 = np.quantile(local_image, (0.25, 0.75))
+        std = (q75-q25)/1.34896
+        z_actual = (image_data[seed_pt[0], seed_pt[1]]-mu)/std
+
+        if z_actual < z_score:
+            # must be the whole field of view
+            assert r0 == 0
+            assert r1 == image_data.shape[0]
+            assert c0 == 0
+            assert c1 == image_data.shape[1]
+        else:
+            if window != window_min:
+                # calculate the z-score associated with the previous window
+                w = window_min
+                while w < window:
+                    window_less = w
+                    w = 3*w//2
+                r0 = max(0, seed_pt[0]-window_less)
+                r1 = min(image_data.shape[0], seed_pt[0]+window_less+1)
+                c0 = max(0, seed_pt[1]-window_less)
+                c1 = min(image_data.shape[1], seed_pt[1]+window_less+1)
+                local_image = image_data[r0:r1, c0:c1].flatten()
+                if mask is not None:
+                    local_image = local_image[
+                                    np.logical_not(mask[r0:r1,
+                                                        c0:c1]).flatten()]
+
+                mu = np.mean(local_image)
+                q25, q75 = np.quantile(local_image, (0.25, 0.75))
+                std = (q75-q25)/1.34896
+                z_less = (image_data[seed_pt[0], seed_pt[1]]-mu)/std
+                assert z_less < z_score
+                found_z += 1
+
+    assert found_z > 0

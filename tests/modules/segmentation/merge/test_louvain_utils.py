af9f17559f7ee37dd2a7f0d3eaf43ce32a1b8d9b
danielsf <scott.daniel@alleninstitute.org>
2021-08-25 10:05:15 -0700

    add test_louvain_utils.py

---
+++ tests/modules/segmentation/merge/test_louvain_utils.py
@@ -0,0 +1,67 @@
+import pytest
+import h5py
+import numpy as np
+import pathlib
+import multiprocessing
+
+from ophys_etl.modules.segmentation.merge.louvain_utils import (
+    _correlation_worker)
+
+
+@pytest.mark.parametrize(
+    "pixel_range, filter_fraction",
+    [((5, 11), 0.2),
+     ((7, 16), 0.3)]
+)
+def test_correlation_worker(tmpdir, pixel_range, filter_fraction):
+    """
+    Test that a single _correlation_worker writes what is expected
+    to the scratch file
+    """
+    tmpdir_path = pathlib.Path(tmpdir)
+    tmpfile_path = tmpdir_path / 'test_corr_worker.h5'
+
+    n_pixels = 20
+    dataset_name = 'silly'
+    with h5py.File(tmpfile_path, 'w') as out_file:
+        out_file.create_dataset(dataset_name,
+                                data=np.zeros((n_pixels, n_pixels),
+                                              dtype=float),
+                                dtype=float)
+
+    rng = np.random.default_rng(762332)
+    sub_video = rng.random((1000, n_pixels))
+
+    mgr = multiprocessing.Manager()
+    lock = mgr.Lock()
+
+    _correlation_worker(
+        sub_video,
+        filter_fraction,
+        pixel_range,
+        lock,
+        tmpfile_path,
+        dataset_name)
+
+    expected = np.zeros((n_pixels, n_pixels), dtype=float)
+    for i0 in range(pixel_range[0], pixel_range[1]):
+        trace0 = sub_video[:, i0]
+        th = np.quantile(trace0, 1.0-filter_fraction)
+        time0 = np.where(trace0 >= th)
+        for i1 in range(i0+1, sub_video.shape[1]):
+            trace1 = sub_video[:, i1]
+            th = np.quantile(trace1, 1.0-filter_fraction)
+            time1 = np.where(trace1 >= th)
+            timestamps = np.unique(np.concatenate([time0, time1]))
+            f0 = trace0[timestamps]
+            mu0 = np.mean(f0)
+            var0 = np.var(f0, ddof=1)
+            f1 = trace1[timestamps]
+            mu1 = np.mean(f1)
+            var1 = np.var(f1, ddof=1)
+            num = np.mean((f0-mu0)*(f1-mu1))
+            expected[i0, i1] = num/np.sqrt(var1*var0)
+
+    with h5py.File(tmpfile_path, 'r') as in_file:
+        actual = in_file[dataset_name][()]
+    np.testing.assert_allclose(actual, expected, rtol=1.0e-10, atol=1.0e-10)

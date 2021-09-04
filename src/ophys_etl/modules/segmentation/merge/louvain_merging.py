379a0bb90400eb41d79056b06ddcaad05029397a
danielsf <scott.daniel@alleninstitute.org>
2021-09-03 17:37:36 -0700

    do_louvain_merging accepts kernel_size

--- src/ophys_etl/modules/segmentation/merge/louvain_merging.py
+++ src/ophys_etl/modules/segmentation/merge/louvain_merging.py
@@ -5,6 +5,9 @@ import numpy as np
 import logging
 import time
 
+from ophys_etl.utils.array_utils import (
+    pairwise_distances)
+
 from ophys_etl.modules.decrosstalk.ophys_plane import OphysROI
 
 from ophys_etl.modules.segmentation.utils.roi_utils import (
@@ -24,6 +27,7 @@ logging.basicConfig(level=logging.INFO)
 def do_louvain_clustering_on_rois(
         roi_list: List[OphysROI],
         full_video: np.ndarray,
+        kernel_size: Union[float, None],
         filter_fraction: float,
         n_processors: int,
         scratch_dir: pathlib.Path) -> Tuple[List[OphysROI],
@@ -58,12 +62,21 @@ def do_louvain_clustering_on_rois(
 
     roi_id_array = np.array(roi_id_array)
 
+    if kernel_size is None:
+        pixel_distances = None
+    else:
+        pixel_coord_array = np.array([[p[0], p[1]]
+                                      for p in index_to_pixel])
+        pixel_distances = pairwise_distances(pixel_coord_array)
+
     t0 = time.time()
     correlation_matrix = correlate_all_pixels(
                             sub_video,
                             filter_fraction,
                             n_processors,
-                            scratch_dir)
+                            scratch_dir,
+                            pixel_distances=pixel_distances,
+                            kernel_size=kernel_size)
 
     duration = time.time()-t0
     logger.info(f'calculated correlation matrix in {duration:.2f} seconds')

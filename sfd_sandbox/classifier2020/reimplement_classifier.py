f492f7d84dc234a62578cc8a3c50c035a134ef43
danielsf <scott.daniel@alleninstitute.org>
2021-10-28 12:56:27 -0700

    have 2020 classifier crop images to 60x60 (not 30x30)

--- sfd_sandbox/classifier2020/reimplement_classifier.py
+++ sfd_sandbox/classifier2020/reimplement_classifier.py
@@ -84,7 +84,7 @@ def classify(artifact_dir):
 
     all_transform = transforms.Compose([
         iaa.Sequential([
-            iaa.CenterCropToFixedSize(height=30, width=30)
+            iaa.CenterCropToFixedSize(height=60, width=60)
         ]).augment_image,
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])

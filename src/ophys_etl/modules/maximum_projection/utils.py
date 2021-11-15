6daead439c481b85cf4e073d1e72171adee6b4da
danielsf <scott.daniel@alleninstitute.org>
2021-11-15 13:27:43 -0800

    implement filter_chunk_of_frames using scipy.ndimage

--- src/ophys_etl/modules/maximum_projection/utils.py
+++ src/ophys_etl/modules/maximum_projection/utils.py
@@ -1,87 +1,34 @@
 import numpy as np
-from itertools import product
+import scipy.ndimage as scipy_ndimage
 
 
-def reflect_indexes(
-        raw_indexes: np.ndarray,
-        dim: int) -> np.ndarray:
-    """
-    Take an array of indexes that may go out of bounds for an array's
-    shape and cast them into valid values by reflecting them about
-    the bounds of the array.
-
-    Parameters
-    ----------
-    raw_indexes: np.ndarray
-
-    dim: int
-        Size of array dimension raw_indexes is meant to refer to
-
-    Returns
-    -------
-    reflected_indexes: np.ndarray
-        Values of raw_index that exceed dim will be reflected about dim
-        (i.e. if dim = 9 and raw_index = [9, 10, 11] then reflected index
-        will be [7, 6, 5]; if raw_index = [-1, -2, -3], reflected_index
-        will be [1, 2, 3])
-
-    Note
-    ----
-    Acts on raw_indexes in place
-    """
-
-    if raw_indexes.max() < dim and raw_indexes.min() >= 0:
-        return raw_indexes
-
-    raw_indexes[(raw_indexes < 0)] *= -1
-    too_big = (raw_indexes >= dim)
-    raw_indexes[too_big] *= -1
-    raw_indexes[too_big] += 2*(dim-1)
-
-    return reflect_indexes(raw_indexes, dim)
-
-
-def median_filter(
-        img: np.ndarray,
+def filter_chunk_of_frames(
+        chunk_of_frames: np.ndarray,
         kernel_size: int) -> np.ndarray:
     """
-    Apply a median filter to a single frame in a video.
+    Apply a median filter to a subset of frames
 
     Parameters
     ----------
-    img: np.ndarray
-        The 2D image being filtered
+    chunk_of_frames: np.ndarray
+        array of shape (ntime, nrows, ncols)
 
-    kernel_size:int
-        The size length of the square kernel used to find
-        the median value about each pixel
+    kernel_size: int
+        side length of square kernel to use for median filter
 
     Returns
     -------
-    filtered_img: np.ndarray
+    filtered_frames: np.ndarray
+        (ntime, nrows, ncols) with each frame passed through
+        the median filter
     """
-    img_shape = img.shape
-    img = img.flatten()
 
-    pixel_indexes_1d = np.arange(len(img), dtype=int)
-    pixel_indexes_2d = np.unravel_index(pixel_indexes_1d,
-                                        img_shape)
+    filtered_frames = np.zeros(chunk_of_frames.shape,
+                               dtype=float)
 
-    k0 = -kernel_size//2
-    kernel_1d = np.arange(k0, k0+kernel_size, 1)
-
-    neighbor_array = np.zeros((len(pixel_indexes_1d), kernel_size**2),
-                              dtype=float)
-
-    for i_neighbor, (drow, dcol) in enumerate(product(kernel_1d, kernel_1d)):
-        neighbor_row = reflect_indexes(pixel_indexes_2d[0]+drow,
-                                       img_shape[0])
-        neighbor_col = reflect_indexes(pixel_indexes_2d[1]+dcol,
-                                       img_shape[1])
-
-        neighbor_1d = np.ravel_multi_index([neighbor_row,
-                                            neighbor_col],
-                                           img_shape)
-
-        neighbor_array[:, i_neighbor] = img[neighbor_1d]
-    return np.median(neighbor_array, axis=1).reshape(img_shape)
+    for i_frame in range(filtered_frames.shape[0]):
+        filtered_frames[i_frame, :, :] = scipy_ndimage.median_filter(
+                                             chunk_of_frames[i_frame, :, :],
+                                             size=kernel_size,
+                                             mode='reflect')
+    return filtered_frames
--- tests/modules/maximum_projection/test_max_projection_utils.py
+++ tests/modules/maximum_projection/test_max_projection_utils.py
@@ -1,17 +1,49 @@
 import pytest
 import numpy as np
+from itertools import product
 
 from ophys_etl.modules.maximum_projection.utils import (
-    reflect_indexes)
+    filter_chunk_of_frames)
 
 
-@pytest.mark.parametrize(
-        'raw_indexes, dim, expected',
-        [(np.array([-2, 4, -3, 5, 6, 7]), 6, np.array([2, 4, 3, 5, 4, 3])),
-         (np.array([0, 4, 1, 5, 6]), 6, np.array([0, 4, 1, 5, 4])),
-         (np.array([-1, 4, 3, 5, 6, 7]), 6, np.array([1, 4, 3, 5, 4, 3])),
-         (np.array([-10, 4, -3, 5, 15, 7]), 6, np.array([0, 4, 3, 5, 5, 3]))
-         ])
-def test_reflect_indexes(raw_indexes, dim, expected):
-    reflected_indexes = reflect_indexes(raw_indexes, dim)
-    np.testing.assert_array_equal(reflected_indexes, expected)
+@pytest.mark.parametrize('kernel_size', [3, 5, 7])
+def test_chunk_of_frames(kernel_size):
+    # test will fail on even number kernel sizes;
+    # not entirely sure how ndimage handles kernels that don't
+    # have a center pixel
+
+    rng = np.random.default_rng(17231)
+    chunk_of_frames = rng.random((10, 20, 20))
+    assert chunk_of_frames.shape == (10, 20, 20)
+
+    kernel_1d = np.arange(-np.floor(kernel_size//2),
+                          kernel_size-np.floor(kernel_size//2),
+                          1,
+                          dtype=int)
+
+    assert len(kernel_1d) == kernel_size
+
+    expected = np.zeros(chunk_of_frames.shape, dtype=float)
+    for i_frame in range(chunk_of_frames.shape[0]):
+        for irow in range(chunk_of_frames.shape[1]):
+            for icol in range(chunk_of_frames.shape[2]):
+                neighbors = np.zeros(kernel_size**2, dtype=float)
+                for ineigh, (drow, dcol) in enumerate(product(kernel_1d,
+                                                              kernel_1d)):
+                    r = irow+drow
+                    if r < 0:
+                        r = -1*r - 1
+                    elif r >= chunk_of_frames.shape[1]:
+                        r = 2*(chunk_of_frames.shape[1]-1) - r + 1
+
+                    c = icol+dcol
+                    if c < 0:
+                        c = -1*c - 1
+                    elif c >= chunk_of_frames.shape[2]:
+                        c = 2*(chunk_of_frames.shape[2]-1) - c + 1
+
+                    neighbors[ineigh] = chunk_of_frames[i_frame, r, c]
+                expected[i_frame, irow, icol] = np.median(neighbors)
+
+    actual = filter_chunk_of_frames(chunk_of_frames, kernel_size)
+    np.testing.assert_array_equal(actual, expected)

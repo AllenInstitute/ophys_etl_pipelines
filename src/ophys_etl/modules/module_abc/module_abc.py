2ee2b8b3973281a77ce9045ff49a1046866ee6da
danielsf <scott.daniel@alleninstitute.org>
2021-11-19 14:23:46 -0800

    handle input_metadata and output_metadata separately

--- src/ophys_etl/modules/module_abc/module_abc.py
+++ src/ophys_etl/modules/module_abc/module_abc.py
@@ -36,7 +36,7 @@ def create_hashed_json(parameter_dict):
     for key in key_list:
         value = parameter_dict[key]
         if isinstance(value, dict):
-            output_dict[key] = create_hashed_json(parameter_dict[key])
+            output_dict.update(create_hashed_json(parameter_dict[key]))
         elif isinstance(value, str) or isinstance(value, Path):
             file_path = Path(value)
             local_dict = dict()
@@ -44,9 +44,7 @@ def create_hashed_json(parameter_dict):
             if file_path.is_file():
                 file_hash = file_hash_from_path(file_path)
                 local_dict['hash'] = file_hash
-            output_dict[key] = local_dict
-        else:
-            output_dict[key] = value
+                output_dict[key] = local_dict
 
     return output_dict
 
@@ -59,16 +57,25 @@ class ModuleRunnerABC(ABC):
         raise NotImplementedError
 
     def run(self):
+        self.output_metadata = dict()
         print("SFD calling ABC.run")
         input_metadata = create_hashed_json(self.args)
+        #print('input_metadata')
+        #print(json.dumps(input_metadata, indent=2))
+        self._run()
+        for k in self.output_metadata:
+            if k in input_metadata:
+                input_metadata.pop(k)
         print('input_metadata')
         print(json.dumps(input_metadata, indent=2))
-        self._run()
+        print('output_metadata')
+        print(json.dumps(self.output_metadata, indent=2))
 
     def output(self, d, output_path=None, **json_dump_options):
         output_d = self.get_output_json(d)
         output_metadata = create_hashed_json(d)
-        print('output_metadata')
-        print(json.dumps(output_metadata, indent=2))
+        #print('output_metadata')
+        #print(json.dumps(output_metadata, indent=2))
         super().output(d, output_path=output_path, **json_dump_options)
+        self.output_metadata = output_metadata
         

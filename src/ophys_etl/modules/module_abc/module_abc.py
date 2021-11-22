5fcc5d771713f5f2bb646058b46600494dbddf35
danielsf <scott.daniel@alleninstitute.org>
2021-11-22 14:20:09 -0800

    try to implement logger class to keep track of full workflow

--- src/ophys_etl/modules/module_abc/module_abc.py
+++ src/ophys_etl/modules/module_abc/module_abc.py
@@ -4,10 +4,93 @@ import hashlib
 import json
 import h5py
 import numpy as np
+import copy
 import argschema
 from marshmallow import post_load
 import pkg_resources
 from abc import ABC, abstractmethod
+
+class FlowElement(object):
+
+    def __init__(self, name, salt, called_by):
+        self.called_by = called_by
+        if salt is not None:
+            self.name = f'{name}_{salt}'
+        else:
+            self.name = str(name)
+        if called_by is not None:
+            self.called_by = called_by.name
+        else:
+            self.called_by = None
+        self.called = None
+        self.metadata = dict()
+
+    def call(self, other):
+        if self.called is None:
+            self.called = list()
+        self.called.append(other.name)
+
+    def add_metadata(self, metadata):
+        self.metadata.update(metadata)
+
+    def as_dict(self):
+        value = dict()
+        value['name'] = self.name
+        value['called_by'] = self.called_by
+        if self.called is not None:
+            value['called'] = self.called
+        if len(self.metadata) > 0:
+            value['metadata'] = self.metadata
+
+        return value
+
+
+class FlowLogger(object):
+
+    def __init__(self):
+        self.log = list()
+        self.currently_in = list()
+        self.name_to_process = dict()
+
+
+    def enter(self, name):
+        if str(name) not in self.name_to_process:
+            salt = None
+        else:
+            new_name = name
+            salt = 1
+            while new_name in self.name_to_process:
+                new_name = f'{name}_{salt}'
+                salt += 1
+
+        if len(self.currently_in) == 0:
+            called_by = None
+        else:
+            called_by = self.currently_in[-1]
+        new_process = FlowElement(name, salt, called_by)
+
+        if called_by is not None:
+            self.name_to_process[called_by.name].call(new_process)
+        self.log.append(new_process)
+        self.currently_in.append(new_process)
+        self.name_to_process[new_process.name] = new_process
+        return new_process.name
+
+
+    def add_metadata(self, process_name, metadata):
+        self.name_to_process[process_name].add_metadata(metadata)
+
+
+    def leave(self):
+        if len(self.currently_in) > 0:
+            self.currently_in.pop(-1)
+
+    def return_log(self):
+        output = list()
+        for process in self.log:
+            output.append(process.as_dict())
+        return output
+
 
 
 class OphysEtlBaseSchema(argschema.ArgSchema):
@@ -102,8 +185,8 @@ def get_environment():
             for n, v in zip(package_names, package_versions)]
 
 
-
 class ModuleRunnerABC(ABC):
+    flow_logger = FlowLogger()
 
     @property
     def metadata_fname(self):
@@ -119,6 +202,8 @@ class ModuleRunnerABC(ABC):
         raise NotImplementedError
 
     def run(self):
+        this_process_name = self.flow_logger.enter(type(self))
+
         self.output_metadata = dict()
 
         if self.args['metadata_field'] is not None:
@@ -136,30 +221,34 @@ class ModuleRunnerABC(ABC):
                 if input_metadata[ii]['path'] in output_paths:
                     input_metadata.pop(ii)
 
-            environ = get_environment()
-
             metadata = dict()
-            metadata['environment'] = environ
             metadata['args'] = self.args
             metadata['input_files'] = input_metadata
             metadata['output_files'] = self.output_metadata
 
-            metadata_fname = self.args[self.args['metadata_field']]
-            if metadata_fname.endswith('h5'):
-                with h5py.File(metadata_fname, 'a') as out_file:
-                    assert 'metadata' not in out_file.keys()
-                    out_file.create_dataset(
-                            'metadata',
-                            data=json.dumps(metadata).encode('utf-8'))
-            elif metadata_fname.endswith('json'):
-                with open(metadata_fname, 'rb') as in_file:
-                    data = json.load(in_file)
-                with open(metadata_fname, 'w') as out_file:
-                    out_file.write(json.dumps({'metadata': metadata,
-                                           'data': data}, indent=2))
-            else:
-                raise ValueError("Cannot handle metadata "
-                                 f"file {metadata_fname}")
+            self.flow_logger.add_metadata(this_process_name, metadata)
+
+            if self.args['metadata_field'] is not None:
+                output_metadata = dict()
+                output_metadata['workflow'] = self.flow_logger.return_log()
+                environ = get_environment()
+                output_metadata['environment'] = environ
+                metadata_fname = self.args[self.args['metadata_field']]
+                if metadata_fname.endswith('h5'):
+                    with h5py.File(metadata_fname, 'a') as out_file:
+                        assert 'metadata' not in out_file.keys()
+                        out_file.create_dataset(
+                                'metadata',
+                                data=json.dumps(output_metadata).encode('utf-8'))
+                elif metadata_fname.endswith('json'):
+                    with open(metadata_fname, 'rb') as in_file:
+                        data = json.load(in_file)
+                    with open(metadata_fname, 'w') as out_file:
+                        out_file.write(json.dumps({'metadata': output_metadata,
+                                               'data': data}, indent=2))
+                else:
+                    raise ValueError("Cannot handle metadata "
+                                     f"file {metadata_fname}")
 
 
     def output(self, d, output_path=None, **json_dump_options):

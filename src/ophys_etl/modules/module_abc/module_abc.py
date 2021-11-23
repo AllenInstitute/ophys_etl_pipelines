561aa9f2e7789e11bbaba012cf1f529aa113765d
danielsf <scott.daniel@alleninstitute.org>
2021-11-22 16:36:29 -0800

    move and document prototype functions

--- src/ophys_etl/modules/module_abc/module_abc.py
+++ src/ophys_etl/modules/module_abc/module_abc.py
@@ -12,6 +12,103 @@ from marshmallow import post_load
 import pkg_resources
 from abc import ABC, abstractmethod
 
+
+
+def file_hash_from_path(file_path: Union[str, pathlib.Path]) -> str:
+    """
+    Return the hexadecimal file hash for a file
+
+    Parameters
+    ----------
+    file_path: Union[str, Path]
+        path to a file
+
+    Returns
+    -------
+    str:
+        The file hash (Blake2b; hexadecimal) of the file
+    """
+    hasher = hashlib.md5()
+    with open(file_path, 'rb') as in_file:
+        chunk = in_file.read(1000000)
+        while len(chunk) > 0:
+            hasher.update(chunk)
+            chunk = in_file.read(1000000)
+    return hasher.hexdigest()
+
+
+
+def create_hashed_json(parameter_dict: dict,
+                       to_skip: Optional[set] = None) -> list:
+    """
+    Scan through a dict of argschema args. Identify all files.
+    If they exist, record their MDF5 checksums. Return as a list.
+
+    Prarameters
+    -----------
+    parameter_dict: dict
+        The dict of argschema args to scan
+
+    to_skip: Optional[set]
+        Set of file_paths to ignore (since the checksum of the file
+        in which you are recording the metadata will change as soon
+        as you add the metadata; no point in hashing that)
+
+    Returns
+    -------
+    file_hashes: list
+        List of elements representing file paths and their checksums.
+        Each element is a dict like
+        {'path': 'path/to/file.h5',
+         'hash': 'abcd12345'}
+    """
+    if to_skip is None:
+        to_skip = set()
+    output_list = list()
+    key_list = list(parameter_dict.keys())
+    key_list.sort()
+    for key in key_list:
+        value = parameter_dict[key]
+        if isinstance(value, dict):
+            output_list += create_hashed_json(parameter_dict[key],
+                                              to_skip=to_skip)
+        elif isinstance(value, str) or isinstance(value, pathlib.Path):
+            file_path = pathlib.Path(value)
+            if str(file_path.resolve().absolute()) in to_skip:
+                continue
+            local_dict = dict()
+            local_dict['path'] = str(file_path.resolve().absolute())
+            if file_path.is_file():
+                file_hash = file_hash_from_path(file_path)
+                local_dict['hash'] = file_hash
+                output_list.append(local_dict)
+
+    return output_list
+
+
+
+def get_environment() -> list:
+    """
+    Return a list of dicts representing all of the available packages
+    in the environment. Elements will be of the form
+    {'name': 'my_package_name',
+     'version': '1.2.3'}
+    """
+    package_names = []
+    package_versions = []
+    for p in pkg_resources.working_set:
+        package_names.append(p.project_name)
+        package_versions.append(p.version)
+    package_names = np.array(package_names)
+    package_versions = np.array(package_versions)
+    sorted_dex = np.argsort(package_names)
+    package_names = package_names[sorted_dex]
+    package_versions = package_versions[sorted_dex]
+    return [{'name':n, 'version': v}
+            for n, v in zip(package_names, package_versions)]
+
+
+
 class WorkflowElement(object):
     """
     A class to represent a single element in a workflow.
@@ -202,71 +299,6 @@ class OphysEtlBaseSchema(argschema.ArgSchema):
             msg += "is neither a .h5 or a .json"
             raise ValueError(msg)
         return data
-
-
-def file_hash_from_path(file_path: Union[str, pathlib.Path]) -> str:
-    """
-    Return the hexadecimal file hash for a file
-
-    Parameters
-    ----------
-    file_path: Union[str, Path]
-        path to a file
-
-    Returns
-    -------
-    str:
-        The file hash (Blake2b; hexadecimal) of the file
-    """
-    hasher = hashlib.md5()
-    with open(file_path, 'rb') as in_file:
-        chunk = in_file.read(1000000)
-        while len(chunk) > 0:
-            hasher.update(chunk)
-            chunk = in_file.read(1000000)
-    return hasher.hexdigest()
-
-
-
-def create_hashed_json(parameter_dict, to_skip=None):
-    if to_skip is None:
-        to_skip = set()
-    output_list = list()
-    key_list = list(parameter_dict.keys())
-    key_list.sort()
-    for key in key_list:
-        value = parameter_dict[key]
-        if isinstance(value, dict):
-            output_list += create_hashed_json(parameter_dict[key],
-                                              to_skip=to_skip)
-        elif isinstance(value, str) or isinstance(value, pathlib.Path):
-            file_path = pathlib.Path(value)
-            if str(file_path.resolve().absolute()) in to_skip:
-                continue
-            local_dict = dict()
-            local_dict['path'] = str(file_path.resolve().absolute())
-            if file_path.is_file():
-                file_hash = file_hash_from_path(file_path)
-                local_dict['hash'] = file_hash
-                output_list.append(local_dict)
-
-    return output_list
-
-
-
-def get_environment():
-    package_names = []
-    package_versions = []
-    for p in pkg_resources.working_set:
-        package_names.append(p.project_name)
-        package_versions.append(p.version)
-    package_names = np.array(package_names)
-    package_versions = np.array(package_versions)
-    sorted_dex = np.argsort(package_names)
-    package_names = package_names[sorted_dex]
-    package_versions = package_versions[sorted_dex]
-    return [{'name':n, 'version': v}
-            for n, v in zip(package_names, package_versions)]
 
 
 class ModuleRunnerABC(ABC):

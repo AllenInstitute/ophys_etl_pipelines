15f98785cef254b8575d4328b5a2ae3d66000499
Christopher B. Morrison <chris.morrison@alleninstitute.org>
2022-01-13 13:55:37 -0800

    Change compute_reference behavior to weight by cmax and use grad.

--- src/ophys_etl/modules/suite2p_registration/suite2p_utils.py
+++ src/ophys_etl/modules/suite2p_registration/suite2p_utils.py
@@ -38,10 +38,11 @@ def load_initial_frames(file_path: str,
 
 
 def compute_reference(frames: np.ndarray,
-                      niter: int,
                       maxregshift: float,
                       smooth_sigma: float,
                       smooth_sigma_time: float,
+                      niter: int = 25,
+                      rtol: float = 1e-4,
                       mask_slope_factor: float = 3) -> np.ndarray:
     """Computes a stacked reference image from the input frames.
 
@@ -55,8 +56,6 @@ def compute_reference(frames: np.ndarray,
     ----------
     frames : array-like, (n_frames, nrows, ncols)
         Set of frames to create a reference from.
-    niter : int
-        Number of iterations to perform when creating the reference image.
     maxregshift : float
         Maximum shift allowed as a fraction of the image width or height, which
         ever is longer.
@@ -66,6 +65,8 @@ def compute_reference(frames: np.ndarray,
     smooth_sigma_time : float
         Width of the Gaussian used to smooth between multiple frames by before
         phase correlation.
+    niter : int
+        Number of iterations to perform when creating the reference image.
     mask_slope_factor : int
         Factor to multiply ``smooth_sigma`` by when creating masks for the
         reference image during suite2p phase correlation. These masks down
@@ -80,11 +81,8 @@ def compute_reference(frames: np.ndarray,
     # Get initial reference image from suite2p.
     ref_image = pick_initial_reference(frames)
 
+    previous_value = 0
     for idx in range(niter):
-        # Compute the number of frames to select in creating the reference
-        # image. At most we select half to the input frames.
-        nmax = int(frames.shape[0] * (1. + idx) / (2 * niter))
-
         # rigid Suite2P phase registration.
         ymax, xmax, cmax = phasecorr(
             data=apply_masks(frames,
@@ -96,29 +94,51 @@ def compute_reference(frames: np.ndarray,
             maxregshift=maxregshift,
             smooth_sigma_time=smooth_sigma_time,
         )
-
-        # Find the indexes of the frames that are the most correlated and
-        # select the first nmax.
-        isort = np.argsort(-cmax)[1:nmax]
+        cmax_mean = cmax.mean()
+        cmax_std = cmax.std(ddof=1)
+        cmax_mask = cmax > cmax_mean - cmax_std
 
         # Copy the most correlated frames so we don't shift the original data.
-        max_corr_frames = np.copy(frames[isort])
-        max_corr_xmax = xmax[isort]
-        max_corr_ymax = ymax[isort]
+        max_corr_frames = np.copy(frames[cmax_mask])
+        max_corr_ymax = ymax[cmax_mask]
+        max_corr_xmax = xmax[cmax_mask]
+        max_corr_cmax = cmax[cmax_mask]
         # Apply shift to the copy of the frames.
         for frame, dy, dx in zip(max_corr_frames,
                                  max_corr_ymax,
                                  max_corr_xmax):
             frame[:] = shift_frame(frame=frame, dy=dy, dx=dx)
 
-        # Create a new reference image from the highest correlated data.
-        ref_image = max_corr_frames.mean(axis=0).astype(np.int16)
+        # Create a new reference image from the weighted average of the most
+        # correlated frames weighted by their correlation^2.
+        ref_image = np.average(max_corr_frames,
+                               weights=max_corr_cmax ** 2).astype(np.int16)
         # Shift reference image to position of mean shifts to remove any bulk
         # displacement.
+        ymean = np.average(max_corr_ymax, weights=max_corr_cmax ** 2)
+
         ref_image = shift_frame(
             frame=ref_image,
-            dy=int(np.round(-max_corr_ymax.mean())),
-            dx=int(np.round(-max_corr_xmax.mean()))
+            dy=int(np.round(-np.average(max_corr_ymax,
+                                        weights=max_corr_cmax ** 2))),
+            dx=int(np.round(-np.average(max_corr_xmax,
+                                        weights=max_corr_cmax ** 2)))
         )
 
+        # Compute our stopping criteria.
+        max_y_shift = np.fabs(max_corr_ymax).max()
+        max_x_shift = np.fabs(max_corr_xmax).max()
+        # Compute the gradient over our image outside of the motion boarder.
+        grady, gradx = np.gradient(
+            ref_image[max_y_shift:-max_y_shift, max_x_shift:-max_x_shift])
+        grad_magnitude = np.sqrt(grady ** 2 + gradx ** 2)
+        # Current convergence criteria: Difference between the median and the
+        # 95th percentile.
+        current_quality = np.percentile(grad_magnitude, 95) \
+                          - np.median(grad_magnitude)
+        if fabs(current_quality - previous_value) / previous_value < rtol:
+            break
+        else:
+            previous_value = current_quality
+    print(f"Created reference image in {idx + 1} iterations...")
     return ref_image

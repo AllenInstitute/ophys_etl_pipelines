5bcc1ff50cc33511431d6fc5d4dc0f4890210755
Adam Amster <adam.amster@alleninstitute.org>
2022-02-01 15:59:16 -0500

    refactor

--- src/ophys_etl/modules/denoising/cloud/__main__.py
+++ src/ophys_etl/modules/denoising/cloud/__main__.py
@@ -2,20 +2,18 @@ from pathlib import Path
 
 import argschema
 
-from ophys_etl.modules.denoising.cloud.container.ecr_utils import \
-    build_and_push_container
+from ophys_etl.modules.denoising.cloud.container.ecr import ECRUploader
 
 
 class CloudDenoisingTrainerModule(argschema.ArgSchemaParser):
     def run(self):
-
-        # Note that if nothing changed to the Dockerfile compared with the
-        # same tag on ECR, then nothing is pushed
-        build_and_push_container(
+        ecr_uploader = ECRUploader(
             repository_name='train_deepinterpolation',
-            dockerfile_dir=(Path(__file__).parent / 'container'),
             image_tag='latest',
             profile_name='sandbox'
+        )
+        ecr_uploader.build_and_push_container(
+            dockerfile_dir=(Path(__file__).parent / 'container')
         )
 
 

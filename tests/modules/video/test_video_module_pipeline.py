dcd7321eef5df12fbfd2a4ef7dfa09a948c6517d
danielsf <scott.daniel@alleninstitute.org>
2022-02-03 16:03:12 -0800

    add tests to make sure video output type is validated

--- tests/modules/video/test_video_module_pipeline.py
+++ tests/modules/video/test_video_module_pipeline.py
@@ -102,3 +102,65 @@ def test_side_by_side_video_downsampling(
                 assert arr.dtype == np.uint8
             else:
                 assert arr.dtype == np.uint16
+
+
+def test_single_video_schema_error(
+        tmpdir,
+        video_path_fixture):
+    """
+    This is just a smoke test
+    """
+    output_suffix = '.jpg'
+    video_dtype = 'uint8'
+    kernel_type = 'mean'
+    tmp_dir = pathlib.Path(tmpdir)
+    video_path = str(video_path_fixture.resolve().absolute())
+    output_path = pathlib.Path(tempfile.mkstemp(dir=tmpdir,
+                                                suffix=output_suffix)[1])
+    input_args = {'video_path': video_path,
+                  'output_path': str(output_path.resolve().absolute()),
+                  'kernel_size': 5,
+                  'input_frame_rate_hz': 12.0,
+                  'output_frame_rate_hz': 7.0,
+                  'reticle': True,
+                  'lower_quantile': 0.1,
+                  'upper_quantile': 0.7,
+                  'tmp_dir': str(tmp_dir.resolve().absolute()),
+                  'quality': 6,
+                  'speed_up_factor': 2,
+                  'n_parallel_workers': 3,
+                  'video_dtype': video_dtype,
+                  'kernel_type': kernel_type}
+
+    with pytest.raises(ValueError, match='output_path must have'):
+        _ = VideoGenerator(input_data=input_args, args=[])
+
+
+def test_side_by_side_video_schema_error(
+        tmpdir,
+        video_path_fixture):
+    output_suffix = '.jpg'
+    video_dtype = 'uint8'
+    kernel_type = 'mean'
+    tmp_dir = pathlib.Path(tmpdir)
+    video_path = str(video_path_fixture.resolve().absolute())
+    output_path = pathlib.Path(tempfile.mkstemp(dir=tmpdir,
+                                                suffix=output_suffix)[1])
+    input_args = {'left_video_path': video_path,
+                  'right_video_path': video_path,
+                  'output_path': str(output_path.resolve().absolute()),
+                  'kernel_size': 5,
+                  'input_frame_rate_hz': 12.0,
+                  'output_frame_rate_hz': 7.0,
+                  'reticle': True,
+                  'lower_quantile': 0.1,
+                  'upper_quantile': 0.7,
+                  'tmp_dir': str(tmp_dir.resolve().absolute()),
+                  'quality': 6,
+                  'speed_up_factor': 2,
+                  'n_parallel_workers': 3,
+                  'video_dtype': video_dtype,
+                  'kernel_type': kernel_type}
+
+    with pytest.raises(ValueError, match='output_path must have'):
+        _ = SideBySideVideoGenerator(input_data=input_args, args=[])

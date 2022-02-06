156dc99d696a81d27f5e1bf862b9fb524fb0b269
Adam Amster <adam.amster@alleninstitute.org>
2022-02-06 15:36:43 -0500

    debug

--- src/ophys_etl/modules/denoising/cloud/cli/__main__.py
+++ src/ophys_etl/modules/denoising/cloud/cli/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 
 import argschema
@@ -9,18 +10,13 @@ from ophys_etl.modules.denoising.cloud.cli.schemas import \
     CloudDenoisingTrainerSchema
 
 
-def _get_dockerfile_dir():
-    import ophys_etl
-    p = Path(ophys_etl.__file__).parent
-    return p / 'modules' / 'denoising' / 'cloud' / 'container'
-
-
 class CloudDenoisingTrainerModule(argschema.ArgSchemaParser):
     default_schema = CloudDenoisingTrainerSchema
     _logger = logging.getLogger(__name__)
-    _container_path = _get_dockerfile_dir()
+    _container_path = Path(__file__).parent.parent / 'container'
 
     def run(self):
+        print(os.listdir(self._container_path))
         repository_name = self.args['docker_params']['repository_name']
         image_tag = self.args['docker_params']['image_tag']
 

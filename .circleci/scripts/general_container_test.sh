0d98ebaaa56743450f657525054738f2583ebaea
danielsf <scott.daniel@alleninstitute.org>
2022-02-11 08:22:00 -0800

    hack everything to run general tests in a docker image

--- .circleci/scripts/general_container_test.sh
+++ .circleci/scripts/general_container_test.sh
@@ -2,7 +2,7 @@ set -e
 export COVERAGE_FILE=/tmp/.coverage
 cd /repos/ophys_etl/
 /envs/ophys_etl/bin/python -m \
-    pytest -m "not suite2p_only and not event_detect_only" \
+    pytest -m "suite2p_only and not event_detect_only" \
     --cov-report xml:/coverage_outputs/general_cov.xml \
     --cov ophys_etl \
     tests

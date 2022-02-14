c3ec83b538a6e844af09ace4380cf5aaf60d0ca1
danielsf <scott.daniel@alleninstitute.org>
2022-02-14 14:35:20 -0800

    crank up verbosity on Docker tests
    want to see if coverage.py was throwing errors here as well

--- .circleci/scripts/event_container_test.sh
+++ .circleci/scripts/event_container_test.sh
@@ -2,7 +2,7 @@ set -e
 export COVERAGE_FILE=/tmp/.coverage
 cd /repos/ophys_etl/
 /envs/event_detection/bin/python -m \
-    pytest -m "event_detect_only" \
+    pytest --verbose -s -m "event_detect_only" \
     --cov-report xml:/coverage_outputs/event_cov.xml \
     --cov ophys_etl \
     tests

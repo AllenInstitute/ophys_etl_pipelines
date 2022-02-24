bceb46e21a0fe72a698d688d3f8ad10fe19ba8c7
danielsf <scott.daniel@alleninstitute.org>
2022-02-24 10:11:31 -0800

    start creating class to read ScanImageMetadata

---
+++ src/ophys_etl/modules/mesoscope_splitting_2022/tiff_metadata.py
@@ -0,0 +1,61 @@
+from typing import List
+import tifffile
+import copy
+import pathlib
+
+
+class ScanImageMetadata(object):
+    """
+    A class to handle reading and parsing the metadata that
+    comes with the TIFF files produced by ScanImage
+
+    Parameters
+    ----------
+    tiff_path: pathlib.Path
+        Path to the TIFF file whose metadata we are parsing
+    """
+
+    def __init__(self, tiff_path: pathlib.Path):
+        if not tiff_path.is_file():
+            raise ValueError(f"{tiff_path.resolve().absolute()} "
+                             "is not a file")
+        self._metadata = tifffile.read_scanimage_metadata(open(tiff_path, 'rb'))
+
+    @property
+    def defined_rois(self) -> List[dict]:
+        """
+        Get the ROIs defined in this TIFF file
+
+        This is list of dicts, each dict containing the ScanImage
+        metadata for a given ROI
+
+        In this context, an ROI is a 3-dimensional volume of the brain
+        that was scanned by the microscope.
+        """
+        if not hasattr(self, '_defined_rois'):
+            roi_parent = self._metadata[1]['RoiGroups']
+            roi_group = roi_parent['imagingRoiGroup']['rois']
+            if isinstance(roi_group, dict):
+                self._defined_rois = [roi_group]
+            elif isinstance(roi_group, list):
+                self._defined_rois = roi_group
+            else:
+                msg = "unable to parse "
+                msg += "self._metadata[1]['RoiGroups']"
+                msg += "['imagingROIGroup']['rois'] "
+                msg += f"of type {type(roi_group)}"
+                raise RuntimeError(msg)
+
+        # use copy to make absolutely sure self._defined_rois
+        # is not accidentally changed downstream
+        return copy.deepcopy(self._defined_rois)
+
+
+    @property
+    def n_rois(self) -> int:
+        """
+        Number of ROIs defined in the metadata for this TIFF file.
+        """
+        if not hasattr(self, '_n_rois'):
+            self._n_rois = len(self.defined_rois)
+        return self._n_rois

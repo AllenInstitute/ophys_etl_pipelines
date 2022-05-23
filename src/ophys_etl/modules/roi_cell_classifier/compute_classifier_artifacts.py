fix normalization

src/ophys_etl/modules/roi_cell_classifier/compute_classifier_artifacts.py
@@ -200,8 +200,12 @@ class ClassifierArtifactsGenerator(ArgSchemaParser):
                                  col_indices[0]:col_indices[1]]
         max_activation_thumbnail = normalize_array(
             array=max_activation_thumbnail,
-            lower_cutoff=self.args["low_quantile"],
-            upper_cutoff=self.args["high_quantile"]
+            lower_cutoff=(
+                np.quantile(max_activation_thumbnail,
+                            self.args['low_quantile'])),
+            upper_cutoff=(
+                np.quantile(max_activation_thumbnail,
+                            self.args['high_quantile'])),
         )
 
         # Find if we need to pad the image.
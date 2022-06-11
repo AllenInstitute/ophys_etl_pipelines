e66c90ac420c1f496e7ada672a837907a8169bef
Christopher B. Morrison <chris.morrison@alleninstitute.org>
2022-06-10 18:25:24 -0700

    add command line options.

--- src/ophys_etl/scripts/ssf_post_processing.py
+++ src/ophys_etl/scripts/ssf_post_processing.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import logging
 import os
@@ -17,19 +18,19 @@ TRACE_DATA_BASE_PATH = pathlib.Path(
     'traces_2022/')
 
 
-def create_trace_input_json(experiment_id: int) -> Path:
+def create_trace_input_json(output_dir: Path, experiment_id: int) -> Path:
     """
     """
-    # Setup output and input files
-    trace_output_dir = str(TRACE_DATA_BASE_PATH / str(experiment_id))
+    # Setup output dir.
+    trace_output_dir = str(output_dir / "traces_2022" / str(experiment_id))
     if not trace_output_dir.exists():
         logging.info(f'Creating Trace output dir {str(trace_output_dir)}')
-        os.mkdir(trace_output_dir)
+        os.makedirs(trace_output_dir)
     else:
         logging.info(f'Using Trace output dir {str(trace_output_dir)}')
-        os.mkdir(trace_output_dir)
+    # Put input and output into json.
     input_json = {
-        "storage_directory": str(TRACE_DATA_BASE_PATH / str(experiment_id)),
+        "storage_directory": trace_output_dir,
         "motion_corrected_stack": str(
             MOTION_DATA_BASE_PATH /
             str(experiment_id) /
@@ -40,7 +41,7 @@ def create_trace_input_json(experiment_id: int) -> Path:
                           "x1": 0.0},
         "log_0": str(MOTION_DATA_BASE_PATH /
                      str(experiment_id) /
-                     f"{experiment_id}rigid_motion_transform.csv"),
+                     f"{experiment_id}_rigid_motion_transform.csv"),
     }
 
     # Copy over ROI data and rename some columns.
@@ -59,13 +60,12 @@ def create_trace_input_json(experiment_id: int) -> Path:
                               "mask": roi["mask"],
                               })
     input_json['rois'] = modified_rois
-    input_json_path = (TRACE_DATA_BASE_PATH /
-                       str(experiment_id) /
+    input_json_path = (trace_output_dir /
                        f"{experiment_id}_traces_input.json")
     if input_json_path.exists():
         logging.info('Trace input json exists. Using already created...')
     else:
-        logging.info('Trace input json exists. Using already created...')
+        logging.info('Writing input json...')
         with open(input_json_path, 'w') as jfile:
             json.dump(input_json)
     return input_json_path
@@ -76,4 +76,17 @@ def get_motion_border(motion_data):
 
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser(description='Post-process labeled data.')
+    parser.add_argument('--experiment_id',
+                        type=str,
+                        help='Path to write data to. Script will create '
+                              'sub-directories for each queue.')
+    parser.add_argument('--output_path',
+                        type=str,
+                        help='Path to write data to. Script will create '
+                              'sub-directories for each queue.')
+    args = parser.parse_args()
+
+    base_dir_path = Path(args.output_path)
+    input_json_path = create_trace_input_json(base_dir_path,
+                                              args.experiment_id)

fa7708cb9726d3800639b79bbd07f27c17b36c1d
Christopher B. Morrison <chris.morrison@alleninstitute.org>
2022-06-13 12:16:31 -0700

    Add subprocess and next task json code.

--- src/ophys_etl/scripts/ssf_post_processing.py
+++ src/ophys_etl/scripts/ssf_post_processing.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import pathlib
+import typing
 from subprocess import Popen
 
 
@@ -14,7 +15,9 @@ ROI_DATA_BASE_PATH = pathlib.Path(
     'denoising_labeling_2022/segmentations')
 
 
-def create_trace_input_json(output_dir: pathlib.Path, experiment_id: int) -> pathlib.Path:
+def create_trace_input_json(output_dir: pathlib.Path,
+                            experiment_id: int) -> Tuple[pathlib.Path,
+                                                         pathlib.Path]:
     """
     """
     # Setup output dir.
@@ -28,22 +31,22 @@ def create_trace_input_json(output_dir: pathlib.Path, experiment_id: int) -> pat
     input_json = {
         "storage_directory": str(trace_output_dir),
         "motion_corrected_stack": str(
-            MOTION_DATA_BASE_PATH /
-            str(experiment_id) /
-            f"{experiment_id}_motion_corrected_video.h5"),
+            MOTION_DATA_BASE_PATH
+            / str(experiment_id)
+            / f"{experiment_id}_motion_corrected_video.h5"),
         "motion_border": {"y1": 0.0,
                           "y0": 0.0,
                           "x0": 0.0,
                           "x1": 0.0},
-        "log_0": str(MOTION_DATA_BASE_PATH /
-                     str(experiment_id) /
-                     f"{experiment_id}_rigid_motion_transform.csv"),
+        "log_0": str(MOTION_DATA_BASE_PATH
+                     / str(experiment_id)
+                     / f"{experiment_id}_rigid_motion_transform.csv"),
     }
 
     # Copy over ROI data and rename some columns.
-    with open(ROI_DATA_BASE_PATH /
-              str(experiment_id) /
-              f"{experiment_id}_rois.json", 'r') as jfile:
+    with open(ROI_DATA_BASE_PATH
+              / str(experiment_id)
+              / f"{experiment_id}_rois.json", 'r') as jfile:
         roi_data = json.load(jfile)
     modified_rois = []
     for roi in roi_data:
@@ -56,16 +59,58 @@ def create_trace_input_json(output_dir: pathlib.Path, experiment_id: int) -> pat
                               "mask": roi["mask_matrix"],
                               })
     input_json['rois'] = modified_rois
-    input_json_path = (trace_output_dir /
-                       f"{experiment_id}_traces_input.json")
+    input_json_path = (trace_output_dir
+                       / f"{experiment_id}_traces_input.json")
+    output_json_path = (trace_output_dir
+                        / f"{experiment_id}_traces_output.json")
     if input_json_path.exists():
         logging.info('Trace input json exists. Using already created...')
     else:
-        logging.info('Writing input json...')
+        logging.info('Writing trace input json...')
         with open(input_json_path, 'w') as jfile:
             json.dump(input_json, jfile, indent=2)
-    return input_json_path
-  
+    return input_json_path, output_json_path
+
+
+def create_demix_input_json(
+        output_dir: pathlib.Path,
+        experiment_id: int,
+        trace_output_json_path: pathlib.Path) -> Tuple[pathlib.Path,
+                                                       pathlib.Path]:
+    """
+    """
+    # Setup output dir.
+    demix_output_dir = output_dir / "demix_2022" / str(experiment_id)
+    if not trace_output_dir.exists():
+        logging.info(f'Creating Demix output dir {str(demix_output_dir)}')
+        os.makedirs(demix_output_dir)
+    else:
+        logging.info(f'Using Demix output dir {str(demix_output_dir)}')
+
+    with open(trace_output_json_path) as jfile:
+        trace_output_json = json.load(jfile)
+    input_json = {
+        "movie_h5": str(
+            MOTION_DATA_BASE_PATH
+            / str(experiment_id)
+            / f"{experiment_id}_motion_corrected_video.h5"),
+        "traces_h5": trace_output_json["roi_trace_file"],
+        "output_file": str(demix_output_dir),
+        "roi_masks": trace_output_json["input_parameters"]["rois"]
+    }
+
+    input_json_path = (demix_output_dir
+                       / f"{experiment_id}_demix_input.json")
+    output_json_path = (demix_output_dir
+                        / f"{experiment_id}_demix_output.json")
+    if input_json_path.exists():
+        logging.info('Demix input json exists. Using already created...')
+    else:
+        logging.info('Writing demix input json...')
+        with open(input_json_path, 'w') as jfile:
+            json.dump(input_json, jfile, indent=2)
+    return input_json_path, output_json_path
+
 
 def get_motion_border(motion_data):
     pass
@@ -76,13 +121,22 @@ if __name__ == "__main__":
     parser.add_argument('--experiment_id',
                         type=str,
                         help='Path to write data to. Script will create '
-                              'sub-directories for each queue.')
+                             'sub-directories for each queue.')
     parser.add_argument('--output_path',
                         type=str,
                         help='Path to write data to. Script will create '
-                              'sub-directories for each queue.')
+                             'sub-directories for each queue.')
     args = parser.parse_args()
-
     base_dir_path = pathlib.Path(args.output_path)
-    input_json_path = create_trace_input_json(base_dir_path,
-                                              args.experiment_id)
+
+    trace_input_json_path, trace_output_json_path = create_trace_input_json(
+        base_dir_path, args.experiment_id)
+    job = Popen("python -m allensdk.brain_observatory.ophys.trace_extraction "
+                f"--input_json={str(trace_input_json_path)} "
+                f"--output_json{str(trace_output_json_path)}", shell=True)
+    job.wait()
+
+    demix_input_json_path, demix_output_json_path = create_demix_input_json(
+        base_dir_path, args.experiment_id, trace_output_json_path)
+
+

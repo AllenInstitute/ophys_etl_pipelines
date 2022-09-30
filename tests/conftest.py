ec18747d46c64b5d19f6e6e1266f4efe7ddf453f
danielsf <scott.daniel@alleninstitute.org>
2022-09-30 10:13:53 -0700

    helper_functions.clean_up_dir removes dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ import pandas as pd
 from numpy.random import seed, uniform
 import pytest
 import json
+import shutil
 
 import numpy as np
 from scipy.sparse import coo_matrix
@@ -30,6 +31,10 @@ class HelperFunctions(object):
                     this_path.unlink()
                 except Exception:
                     pass
+        try:
+            shutil.rmtree(tmpdir)
+        except Exception:
+            pass
 
 
 @pytest.fixture

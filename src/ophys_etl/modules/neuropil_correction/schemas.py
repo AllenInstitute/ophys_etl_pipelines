fdfef620bbf6683a37c771b6627bc965f224c3b0
Mike Huang <huangjmike@gmail.com>
2023-03-05 21:18:26 -0800

    add neuropil_correction package
    add neuropil_correction package
    
    add typing/docstring

---
+++ src/ophys_etl/modules/neuropil_correction/schemas.py
@@ -0,0 +1,20 @@
+from argschema import ArgSchema, fields
+from ophys_etl.schemas.fields import H5InputFile
+
+
+class NeuropilCorrectionJobSchema(ArgSchema):
+    neuropil_trace_file = H5InputFile(
+        required=True, description=("Path to input h5 file containing neuropil traces")
+    )
+
+    roi_trace_file = H5InputFile(
+        required=True, description=("Path to input h5 file containing roi traces")
+    )
+
+    storage_directory = fields.OutputDir(
+        required=True, description=("Path to output directory")
+    )
+
+    motion_corrected_stack = H5InputFile(
+        required=True, description=("Path to input h5 file containing neuropil traces")
+    )

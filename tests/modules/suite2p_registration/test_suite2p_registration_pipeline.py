d5470747360b37ca0f5ad0da988b50681c49a678
Christopher B. Morrison <chris.morrison@alleninstitute.org>
2023-03-13 13:38:48 -0700

    isort+black

--- tests/modules/suite2p_registration/test_suite2p_registration_pipeline.py
+++ tests/modules/suite2p_registration/test_suite2p_registration_pipeline.py
@@ -1,21 +1,20 @@
-import pytest
-import h5py
 import pathlib
 import tempfile
 from itertools import product
+
+import h5py
 import numpy as np
 import pandas as pd
+import pytest
+from ophys_etl.modules.suite2p_registration.__main__ import Suite2PRegistration
 
-from ophys_etl.modules.suite2p_registration.__main__ import (
-        Suite2PRegistration)
 
-
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def video_path_fixture(tmpdir_factory):
-    video_dir = pathlib.Path(tmpdir_factory.mktemp('motion_correction_video'))
-    video_path = tempfile.mkstemp(dir=video_dir,
-                                  prefix='video_',
-                                  suffix='.h5')[1]
+    video_dir = pathlib.Path(tmpdir_factory.mktemp("motion_correction_video"))
+    video_path = tempfile.mkstemp(
+        dir=video_dir, prefix="video_", suffix=".h5"
+    )[1]
 
     video_path = pathlib.Path(video_path)
 
@@ -32,12 +31,12 @@ def video_path_fixture(tmpdir_factory):
 
     for rr in (10, 20, 30):
         for cc in (7, 12, 25, 29):
-            amp = rng.random()*5.0+4.0
-            r0 = max(0, rr-3)
-            r1 = min(nrows, rr+3)
-            c0 = max(0, cc-3)
-            c1 = min(ncols, cc+3)
-            roi = np.round(rng.normal(amp, 1.0, (ntime, r1-r0, c1-c0)))
+            amp = rng.random() * 5.0 + 4.0
+            r0 = max(0, rr - 3)
+            r1 = min(nrows, rr + 3)
+            c0 = max(0, cc - 3)
+            c1 = min(ncols, cc + 3)
+            roi = np.round(rng.normal(amp, 1.0, (ntime, r1 - r0, c1 - c0)))
             roi = roi.astype(np.int16)
             data[:, r0:r1, c0:c1] += roi
 
@@ -46,8 +45,8 @@ def video_path_fixture(tmpdir_factory):
         dy = rng.integers(-5, 5)
         data[ii, :, :] = np.roll(data[ii, :, :], (dx, dy), axis=(1, 0))
 
-    with h5py.File(video_path, 'w') as out_file:
-        out_file.create_dataset('data', data=data)
+    with h5py.File(video_path, "w") as out_file:
+        out_file.create_dataset("data", data=data)
     yield video_path
     video_path.unlink()
 
@@ -55,102 +54,107 @@ def video_path_fixture(tmpdir_factory):
 @pytest.mark.parametrize(
     "nonrigid, clip_negative, "
     "do_optimize_motion_params, use_ave_image_as_reference",
-    product((True, False), (True, False), (True, False), (True, False)))
+    product((True, False), (True, False), (True, False), (True, False)),
+)
 def test_suite2p_motion_correction(
-        tmp_path_factory,
-        nonrigid,
-        clip_negative,
-        do_optimize_motion_params,
-        use_ave_image_as_reference,
-        video_path_fixture,
-        helper_functions):
-
-    tmpdir = tmp_path_factory.mktemp('s2p_motion')
+    tmp_path_factory,
+    nonrigid,
+    clip_negative,
+    do_optimize_motion_params,
+    use_ave_image_as_reference,
+    video_path_fixture,
+    helper_functions,
+):
+    tmpdir = tmp_path_factory.mktemp("s2p_motion")
 
     corr_video_path = tempfile.mkstemp(
-                            dir=tmpdir,
-                            prefix='motion_corrected_',
-                            suffix='.h5')[1]
+        dir=tmpdir, prefix="motion_corrected_", suffix=".h5"
+    )[1]
 
     diagnostics_path = tempfile.mkstemp(
-                            dir=tmpdir,
-                            prefix='motion_diagnostics_',
-                            suffix='.csv')[1]
+        dir=tmpdir, prefix="motion_diagnostics_", suffix=".csv"
+    )[1]
 
     max_projection_path = tempfile.mkstemp(
-                            dir=tmpdir,
-                            prefix='max_projection_',
-                            suffix='.png')[1]
+        dir=tmpdir, prefix="max_projection_", suffix=".png"
+    )[1]
 
     avg_projection_path = tempfile.mkstemp(
-                            dir=tmpdir,
-                            prefix='avg_projection_',
-                            suffix='.png')[1]
+        dir=tmpdir, prefix="avg_projection_", suffix=".png"
+    )[1]
 
     summary_path = tempfile.mkstemp(
-                            dir=tmpdir,
-                            prefix='summary_',
-                            suffix='.png')[1]
+        dir=tmpdir, prefix="summary_", suffix=".png"
+    )[1]
 
     webm_path = tempfile.mkstemp(
-                            dir=tmpdir,
-                            prefix='preview_',
-                            suffix='.webm')[1]
+        dir=tmpdir, prefix="preview_", suffix=".webm"
+    )[1]
 
-    output_json = tempfile.mkstemp(dir=tmpdir,
-                                   prefix='output_',
-                                   suffix='.json')[1]
+    output_json = tempfile.mkstemp(
+        dir=tmpdir, prefix="output_", suffix=".json"
+    )[1]
 
     str_tmpdir = str(tmpdir.resolve().absolute())
-    s2p_args = {'nonrigid': nonrigid,
-                'h5py': str(video_path_fixture.resolve().absolute()),
-                'tmp_dir': str_tmpdir,
-                'output_dir': str_tmpdir,
-                'batch_size': 500}
+    s2p_args = {
+        "nonrigid": nonrigid,
+        "h5py": str(video_path_fixture.resolve().absolute()),
+        "tmp_dir": str_tmpdir,
+        "output_dir": str_tmpdir,
+        "batch_size": 500,
+    }
 
-    args = {'suite2p_args': s2p_args,
-            'movie_frame_rate_hz': 6.1,
-            'clip_negative': clip_negative,
-            'do_optimize_motion_params': do_optimize_motion_params,
-            'use_ave_image_as_reference': use_ave_image_as_reference,
-            'smooth_sigma_min': 0.65,
-            'smooth_sigma_max': 1.15,
-            'smooth_sigma_steps': 2,
-            'smooth_sigma_time_min': 0.0,
-            'smooth_sigma_time_max': 2.0,
-            'smooth_sigma_time_steps': 2,
-            'motion_corrected_output': corr_video_path,
-            'motion_diagnostics_output': diagnostics_path,
-            'max_projection_output': max_projection_path,
-            'avg_projection_output': avg_projection_path,
-            'registration_summary_output': summary_path,
-            'motion_correction_preview_output': webm_path,
-            'output_json': output_json}
+    args = {
+        "suite2p_args": s2p_args,
+        "movie_frame_rate_hz": 6.1,
+        "clip_negative": clip_negative,
+        "do_optimize_motion_params": do_optimize_motion_params,
+        "use_ave_image_as_reference": use_ave_image_as_reference,
+        "smooth_sigma_min": 0.65,
+        "smooth_sigma_max": 1.15,
+        "smooth_sigma_steps": 2,
+        "smooth_sigma_time_min": 0.0,
+        "smooth_sigma_time_max": 2.0,
+        "smooth_sigma_time_steps": 2,
+        "motion_corrected_output": corr_video_path,
+        "motion_diagnostics_output": diagnostics_path,
+        "max_projection_output": max_projection_path,
+        "avg_projection_output": avg_projection_path,
+        "registration_summary_output": summary_path,
+        "motion_correction_preview_output": webm_path,
+        "output_json": output_json,
+    }
 
-    runner = Suite2PRegistration(args=[],
-                                 input_data=args)
+    runner = Suite2PRegistration(args=[], input_data=args)
     runner.run()
 
     mo_corr_output = pd.read_csv(diagnostics_path)
     mo_corr_cols = mo_corr_output.columns
-    expected_cols = ['framenumber', 'x', 'y', 'x_pre_clip', 'y_pre_clip',
-                     'correlation', 'is_valid']
+    expected_cols = [
+        "framenumber",
+        "x",
+        "y",
+        "x_pre_clip",
+        "y_pre_clip",
+        "correlation",
+        "is_valid",
+    ]
     if nonrigid:
-        expected_cols.extend(['nonrigid_x', 'nonrigid_y', 'nonrigid_corr'])
+        expected_cols.extend(["nonrigid_x", "nonrigid_y", "nonrigid_corr"])
     np.testing.assert_array_equal(expected_cols, mo_corr_cols.to_list())
 
-    with h5py.File(corr_video_path, 'r') as in_file:
-        corrected_video = in_file['data'][()]
+    with h5py.File(corr_video_path, "r") as in_file:
+        corrected_video = in_file["data"][()]
 
     # check that negative pixels were clipped correctly (or not)
     eps = 1.0e-10
     if clip_negative:
         assert corrected_video.min() >= 0.0
     else:
-        assert corrected_video.min() < -1.0*eps
+        assert corrected_video.min() < -1.0 * eps
 
-    with h5py.File(video_path_fixture, 'r') as in_file:
-        input_video = in_file['data'][()]
+    with h5py.File(video_path_fixture, "r") as in_file:
+        input_video = in_file["data"][()]
 
     # check that rigid/nonrigid motion correction was applied as requested
     # (this is done by ordering the pixels in each frame and making sure,
@@ -172,9 +176,14 @@ def test_suite2p_motion_correction(
     if nonrigid:
         assert n_non_rigid_different > 0
 
-    path_list = (corr_video_path, diagnostics_path,
-                 max_projection_path, avg_projection_path,
-                 summary_path, webm_path)
+    path_list = (
+        corr_video_path,
+        diagnostics_path,
+        max_projection_path,
+        avg_projection_path,
+        summary_path,
+        webm_path,
+    )
     for this_path in path_list:
         this_path = pathlib.Path(this_path)
         if this_path.is_file():

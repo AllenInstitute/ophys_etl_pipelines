dc4aa9d814d353cc00ae64a4aa3554b7bc9d3759
Adam Amster <adam.amster@alleninstitute.org>
2023-06-12 13:23:33 -0400

    skip checking if dir empty in debug mode

--- src/ophys_etl/workflows/db/db_utils.py
+++ src/ophys_etl/workflows/db/db_utils.py
@@ -5,6 +5,7 @@ import os
 from pathlib import Path
 from typing import Callable, Dict, List, Optional, Union
 
+from ophys_etl.workflows.app_config.app_config import app_config
 from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select
 
@@ -243,13 +244,17 @@ def _validate_files_exist(output_files: List[OutputFile]):
         If any of the files don't exist or dir is empty
     """
     for out in output_files:
-        if Path(out.path).is_dir():
-            if len(os.listdir(out.path)) == 0:
-                raise ModuleOutputFileDoesNotExistException(
-                    f"Directory {out.path} is empty"
-                )
         if not Path(out.path).exists():
             raise ModuleOutputFileDoesNotExistException(
                 f"Expected {out.well_known_file_type} to "
                 f"exist at {out.path} but it did not"
             )
+        if Path(out.path).is_dir():
+            if len(os.listdir(out.path)) == 0:
+                if app_config.is_debug:
+                    # Skipping in debug mode as it's possible for no files to
+                    #  get written, which is expected
+                    return
+                raise ModuleOutputFileDoesNotExistException(
+                    f"Directory {out.path} is empty"
+                )

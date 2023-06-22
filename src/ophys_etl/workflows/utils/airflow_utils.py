299afaa236301e76a9409d0717db5cd5db62afe3
Adam Amster <adam.amster@alleninstitute.org>
2023-06-22 12:40:36 -0400

    stagger and retry rest api calls

--- src/ophys_etl/workflows/utils/airflow_utils.py
+++ src/ophys_etl/workflows/utils/airflow_utils.py
@@ -2,6 +2,12 @@ import configparser
 import os
 from pathlib import Path
 
+# Max number of retries to make when attempting to query airflow rest api
+MAX_REST_API_RETRIES = 20
+
+# Seconds to wait before retrying again
+REST_API_RETRY_SECONDS = 10
+
 
 def get_rest_api_port() -> str:
     """Get web server port defined in airflow cfg
--- src/ophys_etl/workflows/utils/dag_utils.py
+++ src/ophys_etl/workflows/utils/dag_utils.py
@@ -1,12 +1,16 @@
 import base64
 import datetime
+import logging
+import time
 from typing import Optional
 
 import requests
 
 from ophys_etl.workflows.app_config.app_config import app_config
-from ophys_etl.workflows.utils.airflow_utils import get_rest_api_port
+from ophys_etl.workflows.utils.airflow_utils import get_rest_api_port, \
+    REST_API_RETRY_SECONDS, MAX_REST_API_RETRIES
 
+logger = logging.getLogger(__name__)
 
 def get_latest_dag_run(
         dag_id: str,
@@ -29,16 +33,30 @@ def get_latest_dag_run(
     auth = base64.b64encode(
         f'{rest_api_username}:{rest_api_password}'.encode('utf-8'))
     rest_api_port = get_rest_api_port()
-    r = requests.get(
-        f'http://0.0.0.0:{rest_api_port}/api/v1/dags/{dag_id}/'
-        f'dagRuns?'
-        'limit=1&'
-        'order_by=-execution_date&'
-        f'state={state}',
-        headers={
-            'Authorization': f'Basic {auth.decode()}'
-        }
-    )
+
+    num_tries = 0
+    url = f'http://0.0.0.0:{rest_api_port}/api/v1/dags/{dag_id}/' \
+          f'dagRuns?limit=1&order_by=-execution_date&state={state}'
+
+    while True:
+        try:
+            r = requests.get(
+                url=url,
+                headers={
+                    'Authorization': f'Basic {auth.decode()}'
+                }
+            )
+            break
+        # This error is sporadically thrown
+        except requests.exceptions.ConnectionError as e:
+            num_tries += 1
+            if num_tries > MAX_REST_API_RETRIES:
+                logger.error('Reached max numb of retries')
+                raise e
+            logger.error(f'Call to {url} failed with {e}. Retrying again in '
+                         f'{REST_API_RETRY_SECONDS} seconds')
+            time.sleep(REST_API_RETRY_SECONDS)
+
     response = r.json()
     if len(response['dag_runs']) == 0:
         last_run_datetime = None

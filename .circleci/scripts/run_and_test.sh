178427d9084e915bade1c781332b77a2a9b1be50
Adam Amster <adam.amster@alleninstitute.org>
2023-08-15 16:57:06 -0400

    explicitly pass coverage path

--- .circleci/scripts/run_and_test.sh
+++ .circleci/scripts/run_and_test.sh
@@ -1,8 +1,9 @@
 PYTEST_MARK=${1}
 PYTHON_VERSION=${2}
-ENV_SPECIFICATION=${3}  # which conda environment to run in
+ENV_SPECIFICATION=${3}  # a name to use for coverage output file
 OUTPUT_XML=${4}    # the name of the coverage xml file (not its full path)
 TEST_DIRECTORY=${5}
+COVERAGE_EXE_PATH=${6:-/envs/${ENV_SPECIFICATION}/bin/coverage}  # path to coverage executable
 
 echo "mark: "${PYTEST_MARK}
 echo "version: "${PYTHON_VERSION}
@@ -14,12 +15,12 @@ set -e
 export COVERAGE_FILE=/tmp/.coverage_${ENV_SPECIFICATION}_${PYTHON_VERSION}
 echo "COVERAGE_FILE set to "${COVERAGE_FILE}
 cd /repos/ophys_etl/
-/envs/${ENV_SPECIFICATION}/bin/coverage run --rcfile .circleci/coveragerc_file --concurrency=multiprocessing -m \
+${COVERAGE_EXE_PATH} run --rcfile .circleci/coveragerc_file --concurrency=multiprocessing -m \
     pytest --verbose \
     -s -m "${PYTEST_MARK}" \
     ${TEST_DIRECTORY}
 
-/envs/${ENV_SPECIFICATION}/bin/coverage combine --data-file=${COVERAGE_FILE} --rcfile .circleci/coveragerc_file
+${COVERAGE_EXE_PATH} combine --data-file=${COVERAGE_FILE} --rcfile .circleci/coveragerc_file
 
 # coverage automatically adds random salt to the end of the
 # coverage file name; need to detect the name of the file
@@ -27,7 +28,3 @@ cd /repos/ophys_etl/
 coverage_file_list=($(ls /tmp/.coverage_${ENV_SPECIFICATION}_${PYTHON_VERSION}*))
 echo "after combining "
 echo ${coverage_file_list[@]}
-
-#/envs/${ENV_SPECIFICATION}/bin/coverage xml --data-file=${COVERAGE_FILE} \
-#-o /coverage_outputs_${PYTHON_VERSION}/${OUTPUT_XML} \
-#--rcfile .circleci/coveragerc_file

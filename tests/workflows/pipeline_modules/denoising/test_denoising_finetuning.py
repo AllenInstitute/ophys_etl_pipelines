cb84a7db43b1326e712935751a66984cabc00278
Mike Huang <huangjmike@gmail.com>
2023-09-10 21:48:57 -0700

    add denoising test class to try
    lint

--- tests/workflows/pipeline_modules/denoising/test_denoising_finetuning.py
+++ tests/workflows/pipeline_modules/denoising/test_denoising_finetuning.py
@@ -10,42 +10,42 @@ try:
     from ophys_etl.workflows.pipeline_modules.denoising.denoising_finetuning import ( # noqa E501
         DenoisingFinetuningModule,
     )
+
+    class TestDenoisingFinetuningModule(BaseTestPipelineModule):
+        def setup(self):
+            super().setup()
+
+        @pytest.mark.deepinterpolation_only
+        @patch.object(OphysSession, "output_dir", new_callable=PropertyMock)
+        @patch.object(
+            DenoisingFinetuningModule, "output_path", new_callable=PropertyMock
+        )
+        def test_inputs(
+            self,
+            mock_output_path,
+            mock_output_dir,
+            temp_dir,
+            mock_ophys_experiment,
+            motion_corrected_ophys_movie_path,
+        ):
+            """Test that inputs are correctly formatted for
+            input into the module."""
+
+            mock_output_path.return_value = temp_dir
+            mock_output_dir.return_value = temp_dir
+
+            mod = DenoisingFinetuningModule(
+                docker_tag="main",
+                ophys_experiment=mock_ophys_experiment,
+                motion_corrected_ophys_movie_file=OutputFile(
+                    well_known_file_type=(
+                        WellKnownFileTypeEnum.MOTION_CORRECTED_IMAGE_STACK
+                    ),
+                    path=motion_corrected_ophys_movie_path,
+                ),
+            )
+            mod.inputs
+
 except ModuleNotFoundError:
     # even though we might skip tests, pytest tries these imports
     pass
-
-
-class TestDenoisingFinetuningModule(BaseTestPipelineModule):
-    def setup(self):
-        super().setup()
-
-    @pytest.mark.deepinterpolation_only
-    @patch.object(OphysSession, "output_dir", new_callable=PropertyMock)
-    @patch.object(
-        DenoisingFinetuningModule, "output_path", new_callable=PropertyMock
-    )
-    def test_inputs(
-        self,
-        mock_output_path,
-        mock_output_dir,
-        temp_dir,
-        mock_ophys_experiment,
-        motion_corrected_ophys_movie_path,
-    ):
-        """Test that inputs are correctly formatted for
-        input into the module."""
-
-        mock_output_path.return_value = temp_dir
-        mock_output_dir.return_value = temp_dir
-
-        mod = DenoisingFinetuningModule(
-            docker_tag="main",
-            ophys_experiment=mock_ophys_experiment,
-            motion_corrected_ophys_movie_file=OutputFile(
-                well_known_file_type=(
-                    WellKnownFileTypeEnum.MOTION_CORRECTED_IMAGE_STACK
-                ),
-                path=motion_corrected_ophys_movie_path,
-            ),
-        )
-        mod.inputs

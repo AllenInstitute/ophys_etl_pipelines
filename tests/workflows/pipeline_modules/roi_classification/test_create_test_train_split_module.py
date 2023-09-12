b0505888cda1494b685ba5a5e72a00c302281bae
Mike Huang <huangjmike@gmail.com>
2023-09-12 02:57:20 -0700

    lint

--- tests/workflows/pipeline_modules/roi_classification/test_create_test_train_split_module.py
+++ tests/workflows/pipeline_modules/roi_classification/test_create_test_train_split_module.py
@@ -1,33 +1,30 @@
+from unittest.mock import PropertyMock, patch
+
 import pytest
-from unittest.mock import patch, PropertyMock
 
+from ophys_etl.workflows.ophys_experiment import OphysSession
+from ophys_etl.workflows.pipeline_modules.roi_classification.create_train_test_split import ( # noqa E501
+    CreateTrainTestSplitModule,
+)
 from tests.workflows.conftest import BaseTestPipelineModule
-
-from ophys_etl.workflows.ophys_experiment import (
-    OphysExperiment,
-    OphysSession)
-from ophys_etl.workflows.output_file import OutputFile
-from ophys_etl.workflows.well_known_file_types import WellKnownFileTypeEnum
-from ophys_etl.workflows.pipeline_modules.roi_classification.create_train_test_split import CreateTrainTestSplitModule  # noqa E501
 
 
 class TestCreateTrainTestSplitModule(BaseTestPipelineModule):
-
     def setup(self):
         super().setup()
 
-    @pytest.skip(reason="this test needs to be completed when the"
-                "CreateTestTrainSplitModule is completed. See ticket"
-                "PSB-192 for more details.")
-    @patch.object(OphysSession, 'output_dir',
-                  new_callable=PropertyMock)
-    @patch.object(CreateTrainTestSplitModule, 'output_path',
-                  new_callable=PropertyMock)
-    def test_inputs(self,
-                    mock_output_path,
-                    mock_output_dir,
-                    mock_thumbnails_dir,
-                    temp_dir):
+    @pytest.skip(
+        reason="this test needs to be completed when the"
+        "CreateTestTrainSplitModule is completed. See ticket"
+        "PSB-192 for more details."
+    )
+    @patch.object(OphysSession, "output_dir", new_callable=PropertyMock)
+    @patch.object(
+        CreateTrainTestSplitModule, "output_path", new_callable=PropertyMock
+    )
+    def test_inputs(
+        self, mock_output_path, mock_output_dir, mock_thumbnails_dir, temp_dir
+    ):
         """Test that inputs are correctly formatted
         for input into the module.
         """
@@ -35,10 +32,8 @@ class TestCreateTrainTestSplitModule(BaseTestPipelineModule):
         mock_output_path.return_value = temp_dir
         mock_output_dir.return_value = temp_dir
 
-
         mod = CreateTrainTestSplitModule(
-            docker_tag='main',
-            thumbnail_dirs=mock_thumbnails_dir
+            docker_tag="main", thumbnail_dirs=mock_thumbnails_dir
         )
 
         mod.inputs
